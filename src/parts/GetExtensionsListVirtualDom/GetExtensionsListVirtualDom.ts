5186c7174964906a5d974bd191c87f60228aa851
Le Vivilet <levivlet@pm.me>
2024-10-26 21:56:10 +0200

    tsc

--- src/parts/GetExtensionsListVirtualDom/GetExtensionsListVirtualDom.ts
+++ src/parts/GetExtensionsListVirtualDom/GetExtensionsListVirtualDom.ts
@@ -1,6 +1,6 @@
 import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
-import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.js'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as GetExtensionsListItemVirtualDom from '../GetExtensionsListItemVirtualDom/GetExtensionsListItemVirtualDom.ts'
 import * as ExtensionStrings from '../ExtensionStrings/ExtensionStrings.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
