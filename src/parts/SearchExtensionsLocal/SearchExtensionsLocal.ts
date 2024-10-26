339370d29242d2fee7e5f281033613b28cbaa2d6
Le Vivilet <levivlet@pm.me>
2024-10-26 19:36:46 +0200

    feature: move matchesParsedValue into separate file

--- src/parts/SearchExtensionsLocal/SearchExtensionsLocal.ts
+++ src/parts/SearchExtensionsLocal/SearchExtensionsLocal.ts
@@ -1,22 +1,11 @@
 import * as ExtensionDisplay from '../ExtensionDisplay/ExtensionDisplay.ts'
+import * as MatchesParsedValue from '../MatchesParsedValue/MatchesParsedValue.ts'
 import * as SortExtensions from '../SortExtensions/SortExtensions.ts'
-
-const matchesParsedValue = (extension: any, parsedValue: any) => {
-  if (extension && typeof extension.name === 'string') {
-    const extensionNameLower = extension.name.toLowerCase()
-    return extensionNameLower.includes(parsedValue.query)
-  }
-  if (extension && typeof extension.id === 'string') {
-    const extensionIdLower = extension.id.toLowerCase()
-    return extensionIdLower.includes(parsedValue.query)
-  }
-  return false
-}
 
 export const getExtensions = async (extensions: any, parsedValue: any) => {
   const filteredExtensions = []
   for (const extension of extensions) {
-    if (matchesParsedValue(extension, parsedValue)) {
+    if (MatchesParsedValue.matchesParsedValue(extension, parsedValue)) {
       filteredExtensions.push({
         name: ExtensionDisplay.getName(extension),
         id: ExtensionDisplay.getId(extension),
