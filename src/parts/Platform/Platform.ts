1d46807c45ececafb5a89642387f4d1630f49a11
Le Vivilet <levivlet@pm.me>
2024-10-26 22:48:59 +0200

    feature: add getPlatform function

--- src/parts/Platform/Platform.ts
+++ src/parts/Platform/Platform.ts
@@ -1,1 +1,27 @@
-export const platform: number = 1
+import * as PlatformType from '../PlatformType/PlatformType.js'
+
+// TODO treeshake this function out
+
+/**
+ * @returns {number}
+ */
+const getPlatform = (): number => {
+  // @ts-ignore
+  if (typeof PLATFORM !== 'undefined') {
+    // @ts-ignore
+    return PLATFORM
+  }
+  if (typeof process !== 'undefined' && process.env.NODE_ENV === 'test') {
+    return PlatformType.Test
+  }
+  // TODO find a better way to pass runtime environment
+  if (typeof name !== 'undefined' && name.endsWith('(Electron)')) {
+    return PlatformType.Electron
+  }
+  if (typeof name !== 'undefined' && name.endsWith('(Web)')) {
+    return PlatformType.Web
+  }
+  return PlatformType.Remote
+}
+
+export const platform: number = getPlatform()
