05e6410d51051266d7028db5cb0c6d6e57320a63
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-28 20:33:13 +0100

    feature: move scrollbarsize function into separate file (#31)

--- src/parts/ScrollBarFunctions/ScrollBarFunctions.ts
+++ src/parts/ScrollBarFunctions/ScrollBarFunctions.ts
@@ -1,20 +1,8 @@
-/**
- *
- * @param {number} size
- * @param {number} contentSize
- * @param {number} minimumSliderSize
- * @returns
- */
-export const getScrollBarSize = (size: number, contentSize: number, minimumSliderSize: number) => {
-  if (size >= contentSize) {
-    return 0
-  }
-  return Math.max(Math.round(size ** 2 / contentSize), minimumSliderSize)
-}
-
 const getScrollBarOffset = (delta: number, finalDelta: number, size: number, scrollBarSize: number) => {
   const scrollBarOffset = (delta / finalDelta) * (size - scrollBarSize)
   return scrollBarOffset
 }
 
 export const getScrollBarY = getScrollBarOffset
+
+export * from '../GetScrollBarSize/GetScrollBarSize.ts'
