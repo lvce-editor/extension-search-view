bf327d01a3cad2199d422a2fe956b7f44abfffd3
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-22 19:38:11 +0100

    feature: add icon helper functions (#61)

--- packages/extension-search-view-worker/src/parts/Icon/Icon.ts
+++ packages/extension-search-view-worker/src/parts/Icon/Icon.ts
@@ -3,3 +3,15 @@ import * as AssetDir from '../AssetDir/AssetDir.ts'
 export const ExtensionDefaultIcon = `${AssetDir.assetDir}/icons/extensionDefaultIcon.png`
 export const ExtensionLanguageBasics = `${AssetDir.assetDir}/icons/language-icon.svg`
 export const ExtensionTheme = `${AssetDir.assetDir}/icons/theme-icon.png`
+
+export const getExtensionDefaultIcon = (assetDir: string): string => {
+  return `${assetDir}/icons/extensionDefaultIcon.png`
+}
+
+export const getExtensionLanguageBasicsIcon = (assetDir: string): string => {
+  return `${assetDir}/icons/language-icon.svg`
+}
+
+export const getExtensionThemeIcon = (assetDir: string): string => {
+  return `${assetDir}/icons/theme-icon.png`
+}
