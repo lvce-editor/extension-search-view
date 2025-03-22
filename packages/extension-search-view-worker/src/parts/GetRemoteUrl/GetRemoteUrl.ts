a786420a87e3e2cd43376ef54e2736e36ecb786e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-22 19:36:34 +0100

    feature: pass platform variable instead of using global variable (#60)

--- packages/extension-search-view-worker/src/parts/GetRemoteUrl/GetRemoteUrl.ts
+++ packages/extension-search-view-worker/src/parts/GetRemoteUrl/GetRemoteUrl.ts
@@ -1,9 +1,8 @@
 import * as AssetDir from '../AssetDir/AssetDir.ts'
-import * as Platform from '../Platform/Platform.ts'
 import * as PlatformType from '../PlatformType/PlatformType.ts'
 
 export const getRemoteUrl = (extension: any, platform: number): string => {
-  if (Platform.platform === PlatformType.Remote || Platform.platform === PlatformType.Electron) {
+  if (platform === PlatformType.Remote || platform === PlatformType.Electron) {
     if (extension.builtin) {
       return `${AssetDir.assetDir}/extensions/${extension.id}/${extension.icon}`
     }
