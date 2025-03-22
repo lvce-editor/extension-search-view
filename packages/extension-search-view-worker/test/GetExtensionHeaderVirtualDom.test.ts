77be3f4b98dd58926b6347f5365a2136c3734c88
Le Vivilet <levivlet@pm.me>
2025-03-22 19:21:36 +0100

    feature: add header dom test

---
+++ packages/extension-search-view-worker/test/GetExtensionHeaderVirtualDom.test.ts
@@ -0,0 +1,38 @@
+import { expect, test } from '@jest/globals'
+import * as GetExtensionHeaderVirtualDom from '../src/parts/GetExtensionHeaderVirtualDom/GetExtensionHeaderVirtualDom.ts'
+
+test('should return correct virtual DOM structure', () => {
+  const placeholder = 'Search extensions...'
+  const actions: readonly any[] = []
+  const result = GetExtensionHeaderVirtualDom.getExtensionHeaderVirtualDom(placeholder, actions)
+  expect(result).toEqual([
+    {
+      childCount: 1,
+      className: 'ExtensionHeader',
+      type: 4,
+    },
+    {
+      childCount: 2,
+      className: 'SearchField',
+      role: 'none',
+      type: 4,
+    },
+    {
+      autocapitalize: 'off',
+      autocorrect: 'off',
+      childCount: 0,
+      className: 'MultilineInputBox',
+      name: 'extensions',
+      onFocus: '',
+      onInput: 'handleExtensionsInput',
+      placeholder: 'Search extensions...',
+      spellcheck: false,
+      type: 62,
+    },
+    {
+      childCount: 0,
+      className: 'SearchFieldButtons',
+      type: 4,
+    },
+  ])
+})
