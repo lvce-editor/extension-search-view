6e8ff9b07c3a1c57ba9dceb6d05c5678b554eb59
Le Vivilet <levivlet@pm.me>
2025-03-22 19:42:26 +0100

    feature: add saveState function

--- packages/extension-search-view-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/extension-search-view-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,6 +1,7 @@
 import * as ClearSearchResults from '../ClearSearchResults/ClearSearchResults.ts'
 import * as GetKeyBindings from '../GetKeyBindings/GetKeyBindings.ts'
 import * as Render from '../Render/Render.ts'
+import * as SaveState from '../SaveState/SaveState.ts'
 import * as SearchExtensions from '../SearchExtensions/SearchExtensions.ts'
 
 export const commandMap = {
@@ -8,4 +9,5 @@ export const commandMap = {
   'SearchExtensions.getKeyBindings': GetKeyBindings.getKeyBindings,
   'SearchExtensions.render': Render.doRender,
   'SearchExtensions.searchExtensions': SearchExtensions.searchExtensions,
+  'SearchExtensions.saveState': SaveState.saveState,
 }
