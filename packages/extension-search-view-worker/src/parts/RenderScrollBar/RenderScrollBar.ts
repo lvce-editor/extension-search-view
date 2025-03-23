8add80c41fcd17680989758524b6b90d2687f5ac
Le Vivilet <levivlet@pm.me>
2025-03-23 18:38:03 +0100

    scrollbar size

--- packages/extension-search-view-worker/src/parts/RenderScrollBar/RenderScrollBar.ts
+++ packages/extension-search-view-worker/src/parts/RenderScrollBar/RenderScrollBar.ts
@@ -1,16 +1,16 @@
 import type { State } from '../State/State.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
-import { getListHeight } from '../GetListHeight/GetListHeight.ts'
-import { getScrollBarSize } from '../GetScrollBarSize/GetScrollBarSize.ts'
+import * as GetListHeight from '../GetListHeight/GetListHeight.ts'
+import * as GetScrollBarSize from '../GetScrollBarSize/GetScrollBarSize.ts'
 import * as Px from '../Px/Px.ts'
 import * as RenderMethod from '../RenderMethod/RenderMethod.ts'
 import * as ScrollBarFunctions from '../ScrollBarFunctions/ScrollBarFunctions.ts'
 
 export const renderScrollBar = (newState: State): readonly any[] => {
-  const listHeight = getListHeight(newState.items.length, newState.itemHeight, newState.height)
+  const listHeight = GetListHeight.getListHeight(newState.items.length, newState.itemHeight, newState.height)
   const total = newState.items.length
   const contentHeight = total * newState.itemHeight
-  const scrollBarHeight = getScrollBarSize(listHeight, contentHeight, newState.minimumSliderSize)
+  const scrollBarHeight = GetScrollBarSize.getScrollBarSize(listHeight, contentHeight, newState.minimumSliderSize)
   const scrollBarY = ScrollBarFunctions.getScrollBarY(newState.deltaY, newState.finalDeltaY, newState.height - newState.headerHeight, scrollBarHeight)
   const roundedScrollBarY = Math.round(scrollBarY)
   const heightString = Px.px(scrollBarHeight)
