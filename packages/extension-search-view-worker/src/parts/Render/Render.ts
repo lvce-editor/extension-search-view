2b0bec896a422a8068ba9de6874948090dd2964f
Le Vivilet <levivlet@pm.me>
2025-03-23 14:45:44 +0100

    feature: add renderHeader function

--- packages/extension-search-view-worker/src/parts/Render/Render.ts
+++ packages/extension-search-view-worker/src/parts/Render/Render.ts
@@ -1,17 +1,14 @@
 import type { State } from '../State/State.ts'
-import * as ActionType from '../ActionType/ActionType.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DiffHeader from '../DiffHeader/DiffHeader.ts'
 import * as DiffItems from '../DiffItems/DiffItems.ts'
 import * as DiffMessage from '../DiffMessage/DiffMessage.ts'
 import * as DiffScrollBar from '../DiffScrollBar/DiffScrollBar.ts'
-import * as ViewletExtensionStrings from '../ExtensionStrings/ExtensionStrings.ts'
-import * as GetExtensionHeaderVirtualDom from '../GetExtensionHeaderVirtualDom/GetExtensionHeaderVirtualDom.ts'
 import * as GetExtensionsVirtualDom from '../GetExtensionsVirtualDom/GetExtensionsVirtualDom.ts'
 import { getScrollBarSize } from '../GetScrollBarSize/GetScrollBarSize.ts'
 import * as GetVisibleExtensions from '../GetVisibleExtensions/GetVisibleExtensions.ts'
-import * as MaskIcon from '../MaskIcon/MaskIcon.ts'
 import * as Px from '../Px/Px.ts'
+import * as RenderHeader from '../RenderHeader/RenderHeader.ts'
 import * as RenderMethod from '../RenderMethod/RenderMethod.ts'
 import * as ScrollBarFunctions from '../ScrollBarFunctions/ScrollBarFunctions.ts'
 
@@ -73,22 +70,8 @@ const renderSearchValue = {
 
 const renderHeader = {
   isEqual: DiffHeader.isEqual,
-  apply(oldState: any, newState: any): any {
-    const actions = [
-      {
-        type: ActionType.Button,
-        title: ViewletExtensionStrings.clearExtensionSearchResults(),
-        icon: `MaskIcon${MaskIcon.ClearAll}`,
-        command: 'Extensions.clearSearchResults',
-      },
-      {
-        type: ActionType.Button,
-        title: ViewletExtensionStrings.filter(),
-        icon: `MaskIcon${MaskIcon.Filter}`,
-      },
-    ]
-    const dom = GetExtensionHeaderVirtualDom.getExtensionHeaderVirtualDom(newState.placeholder, actions)
-    return ['setHeaderDom', dom]
+  apply(oldState: State, newState: State): readonly any[] {
+    return RenderHeader.renderHeader(newState)
   },
 }
 
