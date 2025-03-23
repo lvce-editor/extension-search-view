5add7b84c7a0ab8b00f0979352c1abe094e2cd3a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-23 14:54:37 +0100

    feature: add renderEventListeners (#128)

---
+++ packages/extension-search-view-worker/src/parts/RenderEventListeners/RenderEventListeners.ts
@@ -0,0 +1,17 @@
+import type { DomEventListener } from '../DomEventListener/DomEventListener.ts'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
+
+export const renderEventListeners = (): readonly DomEventListener[] => {
+  return [
+    {
+      name: DomEventListenerFunctions.HandleContextMenu,
+      params: ['handleContextMenu', 'event.button', 'event.clientX', 'event.clientY'],
+      preventDefault: true,
+    },
+    {
+      name: DomEventListenerFunctions.HandleWheel,
+      params: ['handleWheel', 'event.deltaMode', 'event.deltaY'],
+      passive: true,
+    },
+  ]
+}
