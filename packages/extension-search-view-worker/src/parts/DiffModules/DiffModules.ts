423d6a8454bd777ed6db07d0997bc8b69b2a0684
Le Vivilet <levivlet@pm.me>
2025-03-23 14:36:07 +0100

    feature: use diffItems function

--- packages/extension-search-view-worker/src/parts/DiffModules/DiffModules.ts
+++ packages/extension-search-view-worker/src/parts/DiffModules/DiffModules.ts
@@ -1,8 +1,9 @@
 import * as DiffFocus from '../DiffFocus/DiffFocus.ts'
 import * as DiffHeader from '../DiffHeader/DiffHeader.ts'
+import * as DiffItems from '../DiffItems/DiffItems.ts'
 import * as DiffMessage from '../DiffMessage/DiffMessage.ts'
 import * as DiffScrollBar from '../DiffScrollBar/DiffScrollBar.ts'
 
-export const modules = [DiffFocus.isEqual, DiffScrollBar.isEqual, DiffMessage.isEqual, DiffHeader.isEqual]
+export const modules = [DiffFocus.isEqual, DiffScrollBar.isEqual, DiffMessage.isEqual, DiffHeader.isEqual, DiffItems.isEqual]
 
-export const numbers = [DiffFocus.diffType, DiffScrollBar.diffType, DiffMessage.diffType, DiffHeader.diffType]
+export const numbers = [DiffFocus.diffType, DiffScrollBar.diffType, DiffMessage.diffType, DiffHeader.diffType, DiffItems.diffType]
