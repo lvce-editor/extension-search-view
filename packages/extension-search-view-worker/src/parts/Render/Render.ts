fa67ee96d53ce0135ada0632708b3af5144c3efe
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-23 14:35:40 +0100

    feature: use diff functions (#120)

--- packages/extension-search-view-worker/src/parts/Render/Render.ts
+++ packages/extension-search-view-worker/src/parts/Render/Render.ts
@@ -1,6 +1,9 @@
 import type { State } from '../State/State.ts'
 import * as ActionType from '../ActionType/ActionType.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as DiffHeader from '../DiffHeader/DiffHeader.ts'
+import * as DiffItems from '../DiffItems/DiffItems.ts'
+import * as DiffMessage from '../DiffMessage/DiffMessage.ts'
 import * as DiffScrollBar from '../DiffScrollBar/DiffScrollBar.ts'
 import * as ViewletExtensionStrings from '../ExtensionStrings/ExtensionStrings.ts'
 import * as GetExtensionHeaderVirtualDom from '../GetExtensionHeaderVirtualDom/GetExtensionHeaderVirtualDom.ts'
@@ -18,15 +21,7 @@ const getListHeight = (state: State): number => {
 }
 
 const renderExtensions = {
-  isEqual(oldState: any, newState: any): boolean {
-    return (
-      oldState.items === newState.items &&
-      oldState.minLineY === newState.minLineY &&
-      oldState.maxLineY === newState.maxLineY &&
-      oldState.deltaY === newState.deltaY &&
-      oldState.focusedIndex === newState.focusedIndex
-    )
-  },
+  isEqual: DiffItems.isEqual,
   apply(oldState: any, newState: any): any {
     // TODO render extensions incrementally when scrolling
     const visibleExtensions = GetVisibleExtensions.getVisible(newState)
@@ -61,9 +56,7 @@ const renderScrollBar = {
 }
 
 const renderMessage = {
-  isEqual(oldState: any, newState: any): boolean {
-    return oldState.message === newState.message
-  },
+  isEqual: DiffMessage.isEqual,
   apply(oldState: any, newState: any): any {
     return [/* method */ RenderMethod.SetMessage, /* message */ newState.message]
   },
@@ -79,9 +72,7 @@ const renderSearchValue = {
 }
 
 const renderHeader = {
-  isEqual(oldState: any, newState: any): boolean {
-    return oldState.placeholder === newState.placeholder
-  },
+  isEqual: DiffHeader.isEqual,
   apply(oldState: any, newState: any): any {
     const actions = [
       {
