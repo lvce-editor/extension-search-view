b0575d02f0a468e29e951ee7b51625ef1550c4a9
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-23 14:39:33 +0100

    feature: add applyRender function (#122)

---
+++ packages/extension-search-view-worker/src/parts/ApplyRender/ApplyRender.ts
@@ -0,0 +1,11 @@
+import type { State } from '../State/State.ts'
+import * as GetRenderer from '../GetRenderer/GetRenderer.ts'
+
+export const applyRender = (oldState: State, newState: State, diffResult: readonly number[]): readonly any[] => {
+  const commands = []
+  for (const item of diffResult) {
+    const fn = GetRenderer.getRenderer(item)
+    commands.push(fn(oldState, newState))
+  }
+  return commands
+}
