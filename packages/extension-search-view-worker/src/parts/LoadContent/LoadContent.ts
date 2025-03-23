d8e66a3c6108a28add521e818359203b4cfb2233
Le Vivilet <levivlet@pm.me>
2025-03-23 18:40:30 +0100

    feature: use namespace import

--- packages/extension-search-view-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/extension-search-view-worker/src/parts/LoadContent/LoadContent.ts
@@ -2,12 +2,12 @@ import * as ExtensionManagement from '../ExtensionManagement/ExtensionManagement
 import * as ExtensionSearchViewStates from '../ExtensionSearchViewStates/ExtensionSearchViewStates.ts'
 import * as GetViewletSize from '../GetViewletSize/GetViewletSize.ts'
 import * as HandleInput from '../HandleInput/HandleInput.ts'
-import { restoreState } from '../RestoreState/RestoreState.ts'
+import * as RestoreState from '../RestoreState/RestoreState.ts'
 
 export const loadContent = async (uid: number, savedState: unknown): Promise<void> => {
   const { newState } = ExtensionSearchViewStates.get(uid)
   const { width, platform } = newState
-  const { searchValue } = restoreState(savedState)
+  const { searchValue } = RestoreState.restoreState(savedState)
   // TODO just get local extensions on demand (not when query string is already different)
   const allExtensions = await ExtensionManagement.getAllExtensions(platform)
   const size = GetViewletSize.getViewletSize(width)
