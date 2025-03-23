f4d152f4736ec7d057f9bfb1675d3bffe8ac6683
Le Vivilet <levivlet@pm.me>
2025-03-23 15:00:51 +0100

    feature: pass assetDir variable instead of using global variable

--- packages/extension-search-view-worker/src/parts/SearchExtensions/SearchExtensions.ts
+++ packages/extension-search-view-worker/src/parts/SearchExtensions/SearchExtensions.ts
@@ -1,12 +1,11 @@
 import * as ParseExtensionSearchValue from '../ParseExtensionSearchValue/ParseExtensionSearchValue.ts'
-import * as Platform from '../Platform/Platform.ts'
 import * as SearchExtensionsLocal from '../SearchExtensionsLocal/SearchExtensionsLocal.ts'
 import { VError } from '../VError/VError.ts'
 
-export const searchExtensions = async (extensions: readonly any[], value: string, platform: number): Promise<any> => {
+export const searchExtensions = async (extensions: readonly any[], value: string, platform: number, assetDir: string): Promise<any> => {
   try {
     const parsedValue = ParseExtensionSearchValue.parseValue(value)
-    const filteredExtensions = await SearchExtensionsLocal.getExtensions(extensions, parsedValue, platform || Platform.platform)
+    const filteredExtensions = await SearchExtensionsLocal.getExtensions(extensions, parsedValue, platform, assetDir)
     return filteredExtensions
   } catch (error) {
     throw new VError(error, 'Failed to search for extensions')
