e2a85cfc1b4ea0d260cd1eeaf5c89a5de14bf9fc
Le Vivilet <levivlet@pm.me>
2025-03-23 11:35:15 +0100

    feature: add scrollbar move function

---
+++ packages/extension-search-view-worker/src/parts/HandleScrollBarMove/HandleScrollBarMove.ts
@@ -0,0 +1,21 @@
+import { setDeltaY } from '../SetDeltaY/SetDeltaY.ts'
+
+const getNewPercent = (contentHeight: number, scrollBarHeight: number, relativeY: number): number => {
+  if (relativeY <= contentHeight - scrollBarHeight / 2) {
+    // clicked in middle
+    return relativeY / (contentHeight - scrollBarHeight)
+  }
+  // clicked at bottom
+  return 1
+}
+
+export const handleScrollBarMove = (state: any, eventY: number): any => {
+  const { y, headerHeight, handleOffset, finalDeltaY, height, scrollBarHeight } = state
+  const relativeY = eventY - y - headerHeight - handleOffset
+  const contentHeight = height - headerHeight
+  const newPercent = getNewPercent(contentHeight, scrollBarHeight, relativeY)
+  const newDeltaY = newPercent * finalDeltaY
+  return setDeltaY(state, newDeltaY)
+}
+
+export const handleScrollBarThumbPointerMove = handleScrollBarMove
