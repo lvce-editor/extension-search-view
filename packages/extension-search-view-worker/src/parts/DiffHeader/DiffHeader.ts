7385d754bc94ec21fd4e856ef82e78e5aff01593
Le Vivilet <levivlet@pm.me>
2025-03-23 14:29:49 +0100

    feature: use header diff function

--- packages/extension-search-view-worker/src/parts/DiffHeader/DiffHeader.ts
+++ packages/extension-search-view-worker/src/parts/DiffHeader/DiffHeader.ts
@@ -1,4 +1,7 @@
 import type { State } from '../State/State.ts'
+import * as DiffType from '../DiffType/DiffType.ts'
+
+export const diffType = DiffType.RenderHeader
 
 export const isEqual = (oldState: State, newState: State): boolean => {
   return oldState.placeholder === newState.placeholder
--- packages/extension-search-view-worker/src/parts/DiffModules/DiffModules.ts
+++ packages/extension-search-view-worker/src/parts/DiffModules/DiffModules.ts
@@ -1,6 +1,7 @@
 import * as DiffFocus from '../DiffFocus/DiffFocus.ts'
+import * as DiffHeader from '../DiffHeader/DiffHeader.ts'
 import * as DiffScrollBar from '../DiffScrollBar/DiffScrollBar.ts'
 
-export const modules = [DiffFocus.isEqual, DiffScrollBar.isEqual]
+export const modules = [DiffFocus.isEqual, DiffScrollBar.isEqual, DiffHeader.isEqual]
 
-export const numbers = [DiffFocus.diffType, DiffScrollBar.diffType]
+export const numbers = [DiffFocus.diffType, DiffScrollBar.diffType, DiffHeader.diffType]
