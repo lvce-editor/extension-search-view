8a53628bb7536f7f1e6a5e7ddb09eadbeb64f170
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-23 14:27:10 +0100

    feature: register actions renderer (#115)

---
+++ packages/extension-search-view-worker/src/parts/RenderActions/RenderActions.ts
@@ -0,0 +1,14 @@
+import type { Action } from '../Action/Action.ts'
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import * as ExtensionSearchViewStates from '../ExtensionSearchViewStates/ExtensionSearchViewStates.ts'
+import * as GetActionsVirtualDom from '../GetActionsVirtualDom/GetActionsVirtualDom.ts'
+
+export const renderActions = (uid: number): readonly VirtualDomNode[] => {
+  const { oldState, newState } = ExtensionSearchViewStates.get(uid)
+  if (oldState === newState) {
+    return []
+  }
+  const actions: readonly Action[] = []
+  const dom = GetActionsVirtualDom.getActionsVirtualDom(actions)
+  return dom
+}
