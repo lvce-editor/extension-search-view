70ccba6029874bdc5c4c8441c286e9124c39152b
Le Vivilet <levivlet@pm.me>
2025-03-23 11:24:34 +0100

    feature: improve handleFocus function

--- packages/extension-search-view-worker/src/parts/HandleFocus/HandleFocus.ts
+++ packages/extension-search-view-worker/src/parts/HandleFocus/HandleFocus.ts
@@ -1,3 +1,6 @@
-export const handleFocus = (): void => {
-  // TODO
+import * as SetFocus from '../SetFocus/SetFocus.ts'
+import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
+
+export const handleFocus = async (): Promise<void> => {
+  await SetFocus.setFocus(WhenExpression.FocusExtensions)
 }
