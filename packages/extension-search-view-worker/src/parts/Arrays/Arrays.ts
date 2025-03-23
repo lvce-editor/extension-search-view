a6686653c7b243354f9e5cb6d8129633caffff36
Le Vivilet <levivlet@pm.me>
2025-03-23 11:14:36 +0100

    feature: add more array functions

--- packages/extension-search-view-worker/src/parts/Arrays/Arrays.ts
+++ packages/extension-search-view-worker/src/parts/Arrays/Arrays.ts
@@ -1,3 +1,11 @@
 export const toSorted = (array: readonly any[], compare: any): any[] => {
   return [...array].sort(compare)
 }
+
+export const isLastIndex = (array: readonly any[], index: number): boolean => {
+  return index === array.length - 1
+}
+
+export const lastIndex = (array: readonly any[]): number => {
+  return array.length - 1
+}
