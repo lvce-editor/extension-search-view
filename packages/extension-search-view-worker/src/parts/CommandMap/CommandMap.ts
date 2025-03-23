a7ec24a0d8690648e44cc6e7c1245bd9ea691bf5
Le Vivilet <levivlet@pm.me>
2025-03-23 08:48:26 +0100

    feature: add handleBlur function

--- packages/extension-search-view-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/extension-search-view-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,6 +1,7 @@
 import * as ClearSearchResults from '../ClearSearchResults/ClearSearchResults.ts'
 import * as Create from '../Create/Create.ts'
 import * as GetKeyBindings from '../GetKeyBindings/GetKeyBindings.ts'
+import * as HandleBlur from '../HandleBlur/HandleBlur.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
 import * as Render from '../Render/Render.ts'
 import * as SaveState from '../SaveState/SaveState.ts'
@@ -13,5 +14,6 @@ export const commandMap = {
   'SearchExtensions.loadContent': LoadContent.loadContent,
   'SearchExtensions.render': Render.doRender,
   'SearchExtensions.saveState': SaveState.saveState,
+  'SearchExtensions.handleBlur': HandleBlur.handleBlur,
   'SearchExtensions.searchExtensions': SearchExtensions.searchExtensions,
 }
