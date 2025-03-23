6e463054d78e15d042f5637d470f089688b49ade
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-23 14:49:04 +0100

    feature: use renderScrollBar function (#124)
    * feature: use renderScrollBar function
    
    * code
    
    * lint

--- packages/extension-search-view-worker/src/parts/Render/Render.ts
+++ packages/extension-search-view-worker/src/parts/Render/Render.ts
@@ -1,23 +1,15 @@
 import type { State } from '../State/State.ts'
 import * as ActionType from '../ActionType/ActionType.ts'
-import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DiffHeader from '../DiffHeader/DiffHeader.ts'
 import * as DiffItems from '../DiffItems/DiffItems.ts'
 import * as DiffMessage from '../DiffMessage/DiffMessage.ts'
 import * as DiffScrollBar from '../DiffScrollBar/DiffScrollBar.ts'
 import * as ViewletExtensionStrings from '../ExtensionStrings/ExtensionStrings.ts'
 import * as GetExtensionHeaderVirtualDom from '../GetExtensionHeaderVirtualDom/GetExtensionHeaderVirtualDom.ts'
-import { getScrollBarSize } from '../GetScrollBarSize/GetScrollBarSize.ts'
 import * as MaskIcon from '../MaskIcon/MaskIcon.ts'
-import * as Px from '../Px/Px.ts'
 import * as RenderItems from '../RenderItems/RenderItems.ts'
 import * as RenderMethod from '../RenderMethod/RenderMethod.ts'
-import * as ScrollBarFunctions from '../ScrollBarFunctions/ScrollBarFunctions.ts'
-
-const getListHeight = (state: State): number => {
-  const { height, headerHeight } = state
-  return height - headerHeight
-}
+import * as RenderScrollBar from '../RenderScrollBar/RenderScrollBar.ts'
 
 const renderExtensions = {
   isEqual: DiffItems.isEqual,
@@ -28,26 +20,8 @@ const renderExtensions = {
 
 const renderScrollBar = {
   isEqual: DiffScrollBar.isEqual,
-  apply(oldState: any, newState: any): any {
-    // @ts-ignore
-    const listHeight = getListHeight(newState)
-    const total = newState.items.length
-    const contentHeight = total * newState.itemHeight
-    const scrollBarHeight = getScrollBarSize(listHeight, contentHeight, newState.minimumSliderSize)
-    const scrollBarY = ScrollBarFunctions.getScrollBarY(
-      newState.deltaY,
-      newState.finalDeltaY,
-      newState.height - newState.headerHeight,
-      scrollBarHeight,
-    )
-    const roundedScrollBarY = Math.round(scrollBarY)
-    const heightString = Px.px(scrollBarHeight)
-    const translateString = Px.position(0, roundedScrollBarY)
-    let className = ClassNames.ScrollBarThumb
-    if (newState.scrollBarActive) {
-      className += ' ' + ClassNames.ScrollBarThumbActive
-    }
-    return [/* method */ RenderMethod.SetScrollBar, translateString, heightString, className]
+  apply(oldState: State, newState: State): readonly any[] {
+    return RenderScrollBar.renderScrollBar(newState)
   },
 }
 
