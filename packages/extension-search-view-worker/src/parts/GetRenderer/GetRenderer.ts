02138800861d479c6bd426fb6a6848f901ebc7d7
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-23 14:53:36 +0100

    feature: update getRenderer (#127)

--- packages/extension-search-view-worker/src/parts/GetRenderer/GetRenderer.ts
+++ packages/extension-search-view-worker/src/parts/GetRenderer/GetRenderer.ts
@@ -1,7 +1,14 @@
 import type { Renderer } from '../Renderer/Renderer.ts'
+import * as DiffType from '../DiffType/DiffType.ts'
+import * as RenderItems from '../RenderItems/RenderItems.ts'
+import * as RenderMessage from '../RenderMessage/RenderMessage.ts'
 
 export const getRenderer = (diffType: number): Renderer => {
   switch (diffType) {
+    case DiffType.RenderItems:
+      return RenderItems.renderItems
+    case DiffType.RenderMessage:
+      return RenderMessage.renderMessage
     default:
       throw new Error('unknown renderer')
   }
