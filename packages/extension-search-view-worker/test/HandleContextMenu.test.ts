fb413b9cd7eb341242d4ae806e8dbbc515af0e89
Le Vivilet <levivlet@pm.me>
2025-03-24 22:39:34 +0100

    feature: add contextmenu test

---
+++ packages/extension-search-view-worker/test/HandleContextMenu.test.ts
@@ -0,0 +1,9 @@
+import { expect, test } from '@jest/globals'
+import * as CreateDefaultState from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import * as HandleContextMenu from '../src/parts/HandleContextMenu/HandleContextMenu.ts'
+
+test('handleContextMenu returns state', async () => {
+  const state = CreateDefaultState.createDefaultState()
+  const result = await HandleContextMenu.handleContextMenu(state, 0, 100, 200)
+  expect(result).toBe(state)
+})
