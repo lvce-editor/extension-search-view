9b5627d7ca7d25fff67405c04038336c256247c9
Le Vivilet <levivlet@pm.me>
2025-03-24 21:41:13 +0100

    feature: add remote url test

---
+++ packages/extension-search-view-worker/tests/GetRemoteUrl.test.ts
@@ -0,0 +1,33 @@
+import { expect, test } from '@jest/globals'
+import * as GetRemoteUrl from '../src/parts/GetRemoteUrl/GetRemoteUrl'
+import * as PlatformType from '../src/parts/PlatformType/PlatformType'
+
+test('returns empty string for non-remote platforms', () => {
+  const extension = { id: 'test', icon: 'icon.png', builtin: false }
+  const assetDir = '/test/assets'
+  expect(GetRemoteUrl.getRemoteUrl(extension, PlatformType.Web, assetDir)).toBe('')
+})
+
+test('returns correct path for remote builtin extension', () => {
+  const extension = { id: 'test', icon: 'icon.png', builtin: true }
+  const assetDir = '/test/assets'
+  expect(GetRemoteUrl.getRemoteUrl(extension, PlatformType.Remote, assetDir)).toBe('/test/assets/extensions/test/icon.png')
+})
+
+test('returns correct path for remote non-builtin extension', () => {
+  const extension = { id: 'test', icon: 'icon.png', builtin: false, path: 'test/path' }
+  const assetDir = '/test/assets'
+  expect(GetRemoteUrl.getRemoteUrl(extension, PlatformType.Remote, assetDir)).toBe('/remote/test/path/icon.png')
+})
+
+test('returns correct path for electron builtin extension', () => {
+  const extension = { id: 'test', icon: 'icon.png', builtin: true }
+  const assetDir = '/test/assets'
+  expect(GetRemoteUrl.getRemoteUrl(extension, PlatformType.Electron, assetDir)).toBe('/test/assets/extensions/test/icon.png')
+})
+
+test('returns correct path for electron non-builtin extension', () => {
+  const extension = { id: 'test', icon: 'icon.png', builtin: false, path: 'test/path' }
+  const assetDir = '/test/assets'
+  expect(GetRemoteUrl.getRemoteUrl(extension, PlatformType.Electron, assetDir)).toBe('/remote/test/path/icon.png')
+})
