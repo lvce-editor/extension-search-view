7b8380530000b9c40d94f59bb404482126308e08
Le Vivilet <levivlet@pm.me>
2025-03-24 21:45:04 +0100

    type

--- packages/extension-search-view-worker/src/parts/HandleClickAt/HandleClickAt.ts
+++ packages/extension-search-view-worker/src/parts/HandleClickAt/HandleClickAt.ts
@@ -2,7 +2,7 @@ import type { State } from '../State/State.ts'
 import * as GetListIndex from '../GetListIndex/GetListIndex.ts'
 import * as SelectIndex from '../SelectIndex/SelectIndex.ts'
 
-export const handleClickAt = async (state: State, eventX: number, eventY: number): Promise<any> => {
+export const handleClickAt = async (state: State, eventX: number, eventY: number): Promise<State> => {
   const { x, y, itemHeight, deltaY } = state
   const index = GetListIndex.getListIndex(eventX, eventY, x, y, deltaY, itemHeight)
   return SelectIndex.selectIndex(state, index)
