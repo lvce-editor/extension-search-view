7e72a3704832b39dd246d99fd0e437bdb84f2307
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-24 19:44:58 +0100

    feature: add event listener test (#159)

---
+++ packages/extension-search-view-worker/test/RenderEventListeners.test.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@jest/globals'
+import * as DomEventListenerFunctions from '../src/parts/DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import { renderEventListeners } from '../src/parts/RenderEventListeners/RenderEventListeners.ts'
+
+test('renderEventListeners returns array of event listeners', () => {
+  const result = renderEventListeners()
+  expect(result).toEqual([
+    {
+      name: DomEventListenerFunctions.HandleContextMenu,
+      params: ['handleContextMenu', 'event.button', 'event.clientX', 'event.clientY'],
+      preventDefault: true,
+    },
+    {
+      name: DomEventListenerFunctions.HandleWheel,
+      params: ['handleWheel', 'event.deltaMode', 'event.deltaY'],
+      passive: true,
+    },
+  ])
+})
