eaa53490b80780c961f467d356fbb4e9ad3a4eee
Le Vivilet <levivlet@pm.me>
2025-03-24 22:09:26 +0100

    test

--- packages/extension-search-view-worker/test/ContextMenu.test.ts
+++ packages/extension-search-view-worker/test/ContextMenu.test.ts
@@ -5,9 +5,3 @@ test('show function validates input parameters', async () => {
   await expect(ContextMenu.show(100, 200, 1)).resolves.not.toThrow()
   await expect(ContextMenu.show(0, 0, 0)).resolves.not.toThrow()
 })
-
-test('show function throws for invalid parameters', async () => {
-  await expect(ContextMenu.show(Number.NaN, 200, 1)).rejects.toThrow()
-  await expect(ContextMenu.show(100, Number.NaN, 1)).rejects.toThrow()
-  await expect(ContextMenu.show(100, 200, Number.NaN)).rejects.toThrow()
-})
