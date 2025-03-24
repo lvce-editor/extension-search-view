e97516c82cb3ba831ab055b7ed3c66a8488d0934
Le Vivilet <levivlet@pm.me>
2025-03-24 21:42:41 +0100

    feature: add icon dom test

---
+++ packages/extension-search-view-worker/test/GetIconVirtualDom.test.ts
@@ -0,0 +1,25 @@
+import { expect, test } from '@jest/globals'
+import * as GetIconVirtualDom from '../src/parts/GetIconVirtualDom/GetIconVirtualDom.ts'
+import * as VirtualDomElements from '../src/parts/VirtualDomElements/VirtualDomElements.ts'
+import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
+import * as AriaRoles from '../src/parts/AriaRoles/AriaRoles.ts'
+
+test('creates virtual dom node with default div type', () => {
+  const result = GetIconVirtualDom.getIconVirtualDom('test')
+  expect(result).toEqual({
+    type: VirtualDomElements.Div,
+    className: `${ClassNames.MaskIcon} MaskIcontest`,
+    role: AriaRoles.None,
+    childCount: 0,
+  })
+})
+
+test('creates virtual dom node with custom type', () => {
+  const result = GetIconVirtualDom.getIconVirtualDom('test', VirtualDomElements.Button)
+  expect(result).toEqual({
+    type: VirtualDomElements.Button,
+    className: `${ClassNames.MaskIcon} MaskIcontest`,
+    role: AriaRoles.None,
+    childCount: 0,
+  })
+})
