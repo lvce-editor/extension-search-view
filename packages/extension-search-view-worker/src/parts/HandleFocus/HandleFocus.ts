7d2c95947ec972311224318fad8ccb875c1ec30e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-24 17:53:07 +0100

    feature: return value from focus and blur commands (#156)

--- packages/extension-search-view-worker/src/parts/HandleFocus/HandleFocus.ts
+++ packages/extension-search-view-worker/src/parts/HandleFocus/HandleFocus.ts
@@ -1,6 +1,8 @@
+import type { State } from '../State/State.ts'
 import * as SetFocus from '../SetFocus/SetFocus.ts'
 import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
 
-export const handleFocus = async (): Promise<void> => {
+export const handleFocus = async (state: State): Promise<State> => {
   await SetFocus.setFocus(WhenExpression.FocusExtensions)
+  return state
 }
