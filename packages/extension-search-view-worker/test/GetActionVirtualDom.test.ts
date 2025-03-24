1e35a8fb3081aa9b6241618c780b8a3c163ed1e2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-24 22:10:18 +0100

    feature: add action test (#183)
    * feature: add action test
    
    * rename
    
    * lint

---
+++ packages/extension-search-view-worker/test/GetActionVirtualDom.test.ts
@@ -0,0 +1,25 @@
+import { expect, test } from '@jest/globals'
+import * as ActionType from '../src/parts/ActionType/ActionType.ts'
+import * as GetActionVirtualDom from '../src/parts/GetActionVirtualDom/GetActionVirtualDom.ts'
+
+test('returns empty array for unknown action type', () => {
+  const action = {
+    type: 999,
+    id: 'test-id',
+    icon: 'test-icon',
+    command: 'test-command',
+  }
+  expect(GetActionVirtualDom.getActionVirtualDom(action)).toEqual([])
+})
+
+test('returns button virtual dom for button action type', () => {
+  const action = {
+    type: ActionType.Button,
+    id: 'test-id',
+    icon: 'test-icon',
+    command: 'test-command',
+  }
+  const result = GetActionVirtualDom.getActionVirtualDom(action)
+  expect(result).toBeDefined()
+  expect(Array.isArray(result)).toBe(true)
+})
