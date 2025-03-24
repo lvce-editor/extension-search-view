086f478c52d31567e03711544ca91f18774b35ca
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-24 22:13:52 +0100

    feature: add context menu test (#184)
    * feature: add context menu test
    
    * test

---
+++ packages/extension-search-view-worker/test/ContextMenu.test.ts
@@ -0,0 +1,7 @@
+import { expect, test } from '@jest/globals'
+import * as ContextMenu from '../src/parts/ContextMenu/ContextMenu.ts'
+
+test('show function validates input parameters', async () => {
+  await expect(ContextMenu.show(100, 200, 1)).resolves.not.toThrow()
+  await expect(ContextMenu.show(0, 0, 0)).resolves.not.toThrow()
+})
