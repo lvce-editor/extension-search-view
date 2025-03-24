9c0a148cf74234831e7a1b94fd24d77b894e943e
Le Vivilet <levivlet@pm.me>
2025-03-24 22:02:53 +0100

    feature: add command ids test

---
+++ packages/extension-search-view-worker/tests/GetCommandIds.test.ts
@@ -0,0 +1,34 @@
+import { expect, test } from '@jest/globals'
+import * as GetCommandIds from '../src/parts/GetCommandIds/GetCommandIds'
+
+test('returns the commandIds array', () => {
+  expect(GetCommandIds.getCommandIds()).toEqual([
+    'clearSearchResults',
+    'closeSuggest',
+    'focusFirst',
+    'focusLast',
+    'focusNext',
+    'focusNextPage',
+    'focusPrevious',
+    'focusPreviousPage',
+    'handleBlur',
+    'handleClick',
+    'handleContextMenu',
+    'handleDisable',
+    'handleFocus',
+    'handleInput',
+    'handleInstall',
+    'handleScrollBarCaptureLost',
+    'handleScrollBarClick',
+    'handleScrollBarMove',
+    'handleUninstall',
+    'handleWheel',
+    'openSuggest',
+    'render',
+    'saveState',
+    'scrollDown',
+    'selectIndex',
+    'setDeltaY',
+    'toggleSuggest',
+  ])
+})
