3c590ba1fa0ece2327adc4d53f65aaa25a3b44f0
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-24 22:27:14 +0100

    feature: add scrollbar test (#191)
    * feature: add scrollbar test
    
    * lint
    
    * add helper function
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * test
    
    * fix
    
    * space
    
    * lint

---
+++ packages/extension-search-view-worker/src/parts/CreateDefaultState/CreateDefaultState.ts
@@ -0,0 +1,30 @@
+import type { State } from '../State/State.ts'
+
+export const createDefaultState = (): State => {
+  return {
+    allExtensions: [],
+    deltaY: 0,
+    finalDeltaY: 0,
+    focusedIndex: 0,
+    headerHeight: 0,
+    height: 100,
+    itemHeight: 20,
+    items: Array(20).fill(null),
+    maxLineY: 0,
+    message: '',
+    minimumSliderSize: 20,
+    minLineY: 0,
+    negativeMargin: 0,
+    placeholder: '',
+    platform: 0,
+    scrollBarActive: false,
+    scrollBarHeight: 0,
+    searchValue: '',
+    size: 0,
+    width: 0,
+    x: 0,
+    y: 0,
+    handleOffset: 0,
+    assetDir: '',
+  }
+}
---
+++ packages/extension-search-view-worker/test/RenderScrollBar.test.ts
@@ -0,0 +1,26 @@
+import { expect, test } from '@jest/globals'
+import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import * as RenderMethod from '../src/parts/RenderMethod/RenderMethod.ts'
+import * as RenderScrollBar from '../src/parts/RenderScrollBar/RenderScrollBar.ts'
+
+test('renders scrollbar with default state', () => {
+  const state = createDefaultState()
+  const result = RenderScrollBar.renderScrollBar(state)
+  expect(result).toEqual([RenderMethod.SetScrollBar, '0px NaNpx', '25px', ClassNames.ScrollBarThumb])
+})
+
+test('renders active scrollbar', () => {
+  const state = { ...createDefaultState(), scrollBarActive: true }
+  const result = RenderScrollBar.renderScrollBar(state)
+  expect(result).toEqual([RenderMethod.SetScrollBar, '0px NaNpx', '25px', `${ClassNames.ScrollBarThumb} ${ClassNames.ScrollBarThumbActive}`])
+})
+
+test('renders scrollbar with scroll position', () => {
+  const state = { ...createDefaultState(), deltaY: 100, finalDeltaY: 100 }
+  const result = RenderScrollBar.renderScrollBar(state)
+  expect(result[0]).toBe(RenderMethod.SetScrollBar)
+  expect(result[1]).toBe('0px 75px')
+  expect(result[2]).toBe('25px')
+  expect(result[3]).toBe(ClassNames.ScrollBarThumb)
+})
