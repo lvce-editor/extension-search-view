ecc51312f062feb33bfc8c0b394ca046e58edde8
Le Vivilet <levivlet@pm.me>
2025-03-24 22:22:00 +0100

    add helper function

--- packages/extension-search-view-worker/test/RenderScrollBar.test.ts
+++ packages/extension-search-view-worker/test/RenderScrollBar.test.ts
@@ -1,35 +1,8 @@
 import { expect, test } from '@jest/globals'
-import type { State } from '../src/parts/State/State.ts'
 import * as ClassNames from '../src/parts/ClassNames/ClassNames.ts'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import * as RenderMethod from '../src/parts/RenderMethod/RenderMethod.ts'
 import * as RenderScrollBar from '../src/parts/RenderScrollBar/RenderScrollBar.ts'
-
-const createDefaultState = (): State => ({
-  allExtensions: [],
-  deltaY: 0,
-  finalDeltaY: 0,
-  focusedIndex: 0,
-  headerHeight: 0,
-  height: 100,
-  itemHeight: 20,
-  items: Array(20).fill(null),
-  maxLineY: 0,
-  message: '',
-  minimumSliderSize: 20,
-  minLineY: 0,
-  negativeMargin: 0,
-  placeholder: '',
-  platform: 0,
-  scrollBarActive: false,
-  scrollBarHeight: 0,
-  searchValue: '',
-  size: 0,
-  width: 0,
-  x: 0,
-  y: 0,
-  handleOffset: 0,
-  assetDir: '',
-})
 
 test('renders scrollbar with default state', () => {
   const state = createDefaultState()
