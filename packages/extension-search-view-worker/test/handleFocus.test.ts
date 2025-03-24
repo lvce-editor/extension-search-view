614a8753ebededec6bbdaedeae45814017a789ea
Le Vivilet <levivlet@pm.me>
2025-03-24 22:43:55 +0100

    lint

--- packages/extension-search-view-worker/test/handleFocus.test.ts
+++ packages/extension-search-view-worker/test/handleFocus.test.ts
@@ -1,16 +1,15 @@
 import { expect, jest, test } from '@jest/globals'
-import { RendererWorker } from '../src/parts/RpcId/RpcId.ts'
-import * as RpcRegistry from '../src/parts/RpcRegistry/RpcRegistry.ts'
-
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import { handleFocus } from '../src/parts/HandleFocus/HandleFocus.ts'
+import { RendererWorker } from '../src/parts/RpcId/RpcId.ts'
+import * as RpcRegistry from '../src/parts/RpcRegistry/RpcRegistry.ts'
 
 test('handleFocus invokes RPC focus method', async () => {
   const mockFn = jest.fn()
   const mockRpc = {
     invoke: mockFn,
   } as any
-  RpcRegistry.set(RendererWorker, mockRpc as any)
+  RpcRegistry.set(RendererWorker, mockRpc)
   const state = createDefaultState()
   expect(await handleFocus(state)).toBe(state)
   expect(mockRpc.invoke).toHaveBeenCalledWith('focus')
