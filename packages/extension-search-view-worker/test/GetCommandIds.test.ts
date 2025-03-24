0c62488da2e3ebeac3a75ded021b7391045816dc
Le Vivilet <levivlet@pm.me>
2025-03-24 22:05:29 +0100

    rename

--- packages/extension-search-view-worker/test/GetCommandIds.test.ts
+++ packages/extension-search-view-worker/test/GetCommandIds.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from '@jest/globals'
-import * as GetCommandIds from '../src/parts/GetCommandIds/GetCommandIds'
+import * as GetCommandIds from '../src/parts/GetCommandIds/GetCommandIds.ts'
 
 test('returns the commandIds array', () => {
   expect(GetCommandIds.getCommandIds()).toEqual([
