af9b74cf8bf6c0adb3a7d7446ec65a44c371c9d3
Le Vivilet <levivlet@pm.me>
2025-03-25 23:05:43 +0100

    feature: use diffSearchValue function

--- packages/extension-search-view-worker/src/parts/DiffSearchValue/DiffSearchValue.ts
+++ packages/extension-search-view-worker/src/parts/DiffSearchValue/DiffSearchValue.ts
@@ -1,8 +1,9 @@
 import type { State } from '../State/State.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
+import * as InputSource from '../InputSource/InputSource.ts'
 
 export const diffType = DiffType.RenderSearchValue
 
 export const isEqual = (oldState: State, newState: State): boolean => {
-  return oldState.searchValue === newState.searchValue
+  return newState.inputSource === InputSource.User || oldState.searchValue === newState.searchValue
 }
