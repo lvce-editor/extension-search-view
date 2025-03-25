7102165e79451602f7a81f789759cd34da037e8d
Le Vivilet <levivlet@pm.me>
2025-03-25 23:06:15 +0100

    feature: mark initial search value input as programmatic

--- packages/extension-search-view-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/extension-search-view-worker/src/parts/LoadContent/LoadContent.ts
@@ -2,6 +2,7 @@ import * as ExtensionManagement from '../ExtensionManagement/ExtensionManagement
 import * as ExtensionSearchViewStates from '../ExtensionSearchViewStates/ExtensionSearchViewStates.ts'
 import * as GetViewletSize from '../GetViewletSize/GetViewletSize.ts'
 import * as HandleInput from '../HandleInput/HandleInput.ts'
+import * as InputSource from '../InputSource/InputSource.ts'
 import * as RestoreState from '../RestoreState/RestoreState.ts'
 
 export const loadContent = async (uid: number, savedState: unknown): Promise<void> => {
@@ -11,6 +12,6 @@ export const loadContent = async (uid: number, savedState: unknown): Promise<voi
   // TODO just get local extensions on demand (not when query string is already different)
   const allExtensions = await ExtensionManagement.getAllExtensions(platform)
   const size = GetViewletSize.getViewletSize(width)
-  const updatedState = await HandleInput.handleInput({ ...newState, allExtensions, size }, searchValue)
+  const updatedState = await HandleInput.handleInput({ ...newState, allExtensions, size, inputSource: InputSource.Script }, searchValue)
   ExtensionSearchViewStates.set(uid, newState, updatedState)
 }
