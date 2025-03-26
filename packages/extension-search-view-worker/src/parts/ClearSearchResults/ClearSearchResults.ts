0075788385f64ef1af0bcb62184b1a5540ea9315
Le Vivilet <levivlet@pm.me>
2025-03-26 22:35:44 +0100

    test

--- packages/extension-search-view-worker/src/parts/ClearSearchResults/ClearSearchResults.ts
+++ packages/extension-search-view-worker/src/parts/ClearSearchResults/ClearSearchResults.ts
@@ -1,6 +1,6 @@
 import type { State } from '../State/State.ts'
 import * as ViewletExtensionsHandleInput from '../HandleInput/HandleInput.ts'
 
-export const clearSearchResults = (state: State): State => {
+export const clearSearchResults = (state: State): Promise<State> => {
   return ViewletExtensionsHandleInput.handleInput(state, '')
 }
--- packages/extension-search-view-worker/test/ClearSearchResults.test.ts
+++ packages/extension-search-view-worker/test/ClearSearchResults.test.ts
@@ -2,11 +2,11 @@ import { expect, test } from '@jest/globals'
 import * as ClearSearchResults from '../src/parts/ClearSearchResults/ClearSearchResults.ts'
 import * as CreateDefaultState from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 
-test('clearSearchResults clears the search input', () => {
+test('clearSearchResults clears the search input', async () => {
   const state = {
     ...CreateDefaultState.createDefaultState(),
     searchValue: 'test search',
   }
-  const result = ClearSearchResults.clearSearchResults(state)
+  const result = await ClearSearchResults.clearSearchResults(state)
   expect(result.searchValue).toBe('')
 })
