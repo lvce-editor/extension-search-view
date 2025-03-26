16c38de45dde71cd954d616c460f642225bd47c9
Le Vivilet <levivlet@pm.me>
2025-03-26 21:19:08 +0100

    feature: add more exact types for getExtensions

--- packages/extension-search-view-worker/src/parts/SearchExtensionsLocal/SearchExtensionsLocal.ts
+++ packages/extension-search-view-worker/src/parts/SearchExtensionsLocal/SearchExtensionsLocal.ts
@@ -1,9 +1,16 @@
+import type { ExtensionListItem } from '../ExtensionListItem/ExtensionListItem.ts'
+import type { ParsedExtensionSearchValue } from '../ParsedExtensionSearchValue/ParsedExtensionSearchValue.ts'
 import * as ExtensionDisplay from '../ExtensionDisplay/ExtensionDisplay.ts'
 import { getPublisher } from '../GetPublisher/GetPublisher.ts'
 import * as MatchesParsedValue from '../MatchesParsedValue/MatchesParsedValue.ts'
 import * as SortExtensions from '../SortExtensions/SortExtensions.ts'
 
-export const getExtensions = async (extensions: any, parsedValue: any, platform: number, assetDir: string): Promise<readonly any[]> => {
+export const getExtensions = async (
+  extensions: readonly ExtensionListItem[],
+  parsedValue: ParsedExtensionSearchValue,
+  platform: number,
+  assetDir: string,
+): Promise<readonly any[]> => {
   const filteredExtensions = []
   for (const extension of extensions) {
     if (MatchesParsedValue.matchesParsedValue(extension, parsedValue)) {
