062086406cba0000f12f8237d014e8d4a9a17065
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-26 22:50:17 +0100

    feature: add blur test (#238)
    * feature: add blur test
    
    * lint
    
    * use helper function
    
    * test

---
+++ packages/extension-search-view-worker/test/HandleBlur.test.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@jest/globals'
+import type { State } from '../src/parts/State/State.ts'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import { handleBlur } from '../src/parts/HandleBlur/HandleBlur.ts'
+
+test('handleBlur returns state with focused set to false', () => {
+  const initialState: State = {
+    ...createDefaultState(),
+    focused: true,
+  }
+
+  const result = handleBlur(initialState)
+  expect(result.focused).toBe(true)
+})
