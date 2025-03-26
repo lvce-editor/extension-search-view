0fc2e23ff0545b27e237f1ceccd2b81da75607b8
Le Vivilet <levivlet@pm.me>
2025-03-26 23:06:41 +0100

    feature: add renderActions test

---
+++ packages/extension-search-view-worker/test/RenderActions.test.ts
@@ -0,0 +1,19 @@
+import { expect, test } from '@jest/globals'
+import { renderActions } from '../src/parts/RenderActions/RenderActions.js'
+import * as ExtensionSearchViewStates from '../src/parts/ExtensionSearchViewStates/ExtensionSearchViewStates.js'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.js'
+
+test('returns empty array when oldState equals newState', () => {
+  const state = createDefaultState()
+  ExtensionSearchViewStates.set(1, state, state)
+  const result = renderActions(1)
+  expect(result).toEqual([])
+})
+
+test('returns actions virtual dom when states are different', () => {
+  const oldState = createDefaultState()
+  const newState = { ...createDefaultState(), searchValue: 'test' }
+  ExtensionSearchViewStates.set(1, oldState, newState)
+  const result = renderActions(1)
+  expect(Array.isArray(result)).toBe(true)
+})
