88fbcf57dfcd7556f48dbf504fc71eb101d5667c
Le Vivilet <levivlet@pm.me>
2025-03-26 22:41:25 +0100

    feature: add wheel test

---
+++ packages/extension-search-view-worker/test/HandleWheel.test.ts
@@ -0,0 +1,38 @@
+import { test, expect } from '@jest/globals'
+import { handleWheel } from '../src/parts/HandleWheel/HandleWheel.ts'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import { setDeltaY } from '../src/parts/SetDeltaY/SetDeltaY.ts'
+
+test('handleWheel should update deltaY', () => {
+  let state = createDefaultState()
+  state = {
+    ...state,
+    height: 400,
+    finalDeltaY: 1000,
+    itemHeight: 40,
+    items: Array(25).fill(null),
+  }
+  state = setDeltaY(state, 10)
+  const deltaMode = 0
+  const deltaY = 5
+
+  const result = handleWheel(state, deltaMode, deltaY)
+  expect(result.deltaY).toBe(15)
+})
+
+test('handleWheel should handle negative deltaY', () => {
+  let state = createDefaultState()
+  state = {
+    ...state,
+    height: 400,
+    finalDeltaY: 1000,
+    itemHeight: 40,
+    items: Array(25).fill(null),
+  }
+  state = setDeltaY(state, 10)
+  const deltaMode = 0
+  const deltaY = -3
+
+  const result = handleWheel(state, deltaMode, deltaY)
+  expect(result.deltaY).toBe(7)
+})
