7e4e39a02b5b2491d8a2acc679f9f32fd1c4db1f
Le Vivilet <levivlet@pm.me>
2025-03-26 22:37:40 +0100

    lint

--- packages/extension-search-view-worker/test/DiffFocus.test.ts
+++ packages/extension-search-view-worker/test/DiffFocus.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@jest/globals'
-import * as DiffFocus from '../src/parts/DiffFocus/DiffFocus.ts'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import * as DiffFocus from '../src/parts/DiffFocus/DiffFocus.ts'
 
 test('isEqual returns true for any state comparison', () => {
   const oldState = createDefaultState()
