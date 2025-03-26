10e468ea5dbc223ecc48f48b530cceea955d48b2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-26 20:57:40 +0100

    feature: add extensionListItem interface (#220)

---
+++ packages/extension-search-view-worker/src/parts/ExtensionListItem/ExtensionListItem.ts
@@ -0,0 +1,8 @@
+export interface ExtensionListItem {
+  readonly icon: string
+  readonly id: string
+  readonly name: string
+  readonly description: string
+  readonly author: string
+  readonly uri: string
+}
