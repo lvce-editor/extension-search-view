f2d5feccf5c78faf1cadd8ad32c714029a7abc20
Le Vivilet <levivlet@pm.me>
2025-03-26 21:07:42 +0100

    feature: optimize parseSearchValue function

--- packages/extension-search-view-worker/src/parts/ParseExtensionSearchValue/ParseExtensionSearchValue.ts
+++ packages/extension-search-view-worker/src/parts/ParseExtensionSearchValue/ParseExtensionSearchValue.ts
@@ -5,38 +5,50 @@ const RE_PARAM = /@\w+/g
 
 // TODO test sorting and filtering
 export const parseValue = (value: string): ParsedExtensionSearchValue => {
-  const parameters = Object.create(null)
+  let installed = false
+  let enabled = false
+  let disabled = false
+  let builtin = false
+  let sort = ''
+  let id = ''
+  let outdated = false
   // TODO this is not very functional code (assignment)
   const replaced = value.replaceAll(RE_PARAM, (match, by, order) => {
     if (match.startsWith(ExtensionFilterParameter.Installed)) {
-      parameters.installed = true
+      installed = true
     }
     if (match.startsWith(ExtensionFilterParameter.Enabled)) {
-      parameters.enabled = true
+      enabled = true
     }
     if (match.startsWith(ExtensionFilterParameter.Disabled)) {
-      parameters.disabled = true
+      disabled = true
     }
     if (match.startsWith(ExtensionFilterParameter.Builtin)) {
-      parameters.builtin = true
+      builtin = true
     }
     if (match.startsWith(ExtensionFilterParameter.Sort)) {
       // TODO
-      parameters.sort = 'installs'
+      sort = 'installs'
     }
     if (match.startsWith(ExtensionFilterParameter.Id)) {
       // TODO
-      parameters.id = 'abc'
+      id = 'abc'
     }
     if (match.startsWith(ExtensionFilterParameter.Outdated)) {
-      parameters.outdated = true
+      outdated = true
     }
     return ''
   })
-  const isLocal = parameters.enabled || parameters.builtin || parameters.disabled || parameters.outdated || parameters.installed
+  const isLocal = enabled || builtin || disabled || outdated || installed
   return {
     query: replaced,
-    ...parameters,
+    enabled,
+    builtin,
+    disabled,
+    outdated,
+    installed,
+    id,
+    sort,
     isLocal,
   }
 }
