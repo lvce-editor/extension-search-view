1c3a8cc7cb05b80a263a94b51b118408fa716e72
Le Vivilet <levivlet@pm.me>
2025-03-26 21:08:40 +0100

    feature: use visibleItem interface

--- packages/extension-search-view-worker/src/parts/GetExtensionsVirtualDom/GetExtensionsVirtualDom.ts
+++ packages/extension-search-view-worker/src/parts/GetExtensionsVirtualDom/GetExtensionsVirtualDom.ts
@@ -1,7 +1,8 @@
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import type { VisibleItem } from '../VisibleItem/VisibleItem.ts'
 import * as GetExtensionsListVirtualDom from '../GetExtensionsListVirtualDom/GetExtensionsListVirtualDom.ts'
 
-export const getExtensionsVirtualDom = (visibleExtensions: readonly any[]): readonly VirtualDomNode[] => {
+export const getExtensionsVirtualDom = (visibleExtensions: readonly VisibleItem[]): readonly VirtualDomNode[] => {
   const dom = GetExtensionsListVirtualDom.getExtensionsListVirtualDom(visibleExtensions)
   // TODO
   return dom
