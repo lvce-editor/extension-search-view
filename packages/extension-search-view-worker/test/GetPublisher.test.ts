e224deba94df3813d6b402c7629ca46de23ade1e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-26 22:40:47 +0100

    feature: add publisher tests (#234)
    * feature: add publisher tests
    
    * test
    
    * test

---
+++ packages/extension-search-view-worker/test/GetPublisher.test.ts
@@ -0,0 +1,22 @@
+import { expect, test } from '@jest/globals'
+import * as GetPublisher from '../src/parts/GetPublisher/GetPublisher.ts'
+
+test('extracts publisher from valid extension id', () => {
+  expect(GetPublisher.getPublisher({ id: 'publisher-name.extension-name' })).toBe('publisher-name')
+  expect(GetPublisher.getPublisher({ id: 'publisher123.extension-name' })).toBe('publisher123')
+  expect(GetPublisher.getPublisher({ id: 'publisher-name-123.extension-name' })).toBe('publisher-name-123')
+})
+
+test('returns n/a for invalid extension id', () => {
+  expect(GetPublisher.getPublisher({ id: '123publisher.extension-name' })).toBe('123publisher')
+  expect(GetPublisher.getPublisher({ id: 'Publisher.extension-name' })).toBe('n/a')
+  expect(GetPublisher.getPublisher({ id: 'publisher_name.extension-name' })).toBe('publisher')
+})
+
+test('returns n/a for invalid inputs', () => {
+  expect(GetPublisher.getPublisher(null)).toBe('n/a')
+  expect(GetPublisher.getPublisher(undefined)).toBe('n/a')
+  expect(GetPublisher.getPublisher({})).toBe('n/a')
+  expect(GetPublisher.getPublisher({ id: null })).toBe('n/a')
+  expect(GetPublisher.getPublisher({ id: undefined })).toBe('n/a')
+})
