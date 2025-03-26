755198a7812133712c365e4121a02c5c3000134a
Le Vivilet <levivlet@pm.me>
2025-03-26 22:45:11 +0100

    feature: add scrolldown test

---
+++ packages/extension-search-view-worker/test/ScrollDown.test.ts
@@ -0,0 +1,23 @@
+import { jest, test, expect } from '@jest/globals'
+import * as ScrollDown from '../src/parts/ScrollDown/ScrollDown.ts'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+
+test('scrollDown', () => {
+  const state = {
+    ...createDefaultState(),
+    deltaY: 0,
+    finalDeltaY: 100,
+  }
+  const result = ScrollDown.scrollDown(state)
+  expect(result.deltaY).toBe(20)
+})
+
+test('scrollDown should not exceed finalDeltaY', () => {
+  const state = {
+    ...createDefaultState(),
+    deltaY: 90,
+    finalDeltaY: 100,
+  }
+  const result = ScrollDown.scrollDown(state)
+  expect(result.deltaY).toBe(100)
+})
