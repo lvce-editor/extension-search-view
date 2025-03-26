896e8c0ae002b81f2744ec3a4f7ffc8e60e21004
Le Vivilet <levivlet@pm.me>
2025-03-26 22:45:57 +0100

    use helper function

--- packages/extension-search-view-worker/test/HandleBlur.test.ts
+++ packages/extension-search-view-worker/test/HandleBlur.test.ts
@@ -1,35 +1,12 @@
 import { test, expect } from '@jest/globals'
 import type { State } from '../src/parts/State/State.ts'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import { handleBlur } from '../src/parts/HandleBlur/HandleBlur.ts'
 
 test('handleBlur returns state with focused set to false', () => {
   const initialState: State = {
-    allExtensions: [],
-    deltaY: 0,
-    finalDeltaY: 0,
-    focusedIndex: 0,
-    headerHeight: 0,
-    height: 0,
-    itemHeight: 0,
-    items: [],
-    maxLineY: 0,
-    message: '',
-    minimumSliderSize: 0,
-    minLineY: 0,
-    negativeMargin: 0,
-    placeholder: '',
-    platform: 0,
-    scrollBarActive: false,
-    scrollBarHeight: 0,
-    searchValue: '',
-    size: 0,
-    width: 0,
-    x: 0,
-    y: 0,
-    handleOffset: 0,
-    assetDir: '',
+    ...createDefaultState(),
     focused: true,
-    inputSource: 0,
   }
 
   const result = handleBlur(initialState)
