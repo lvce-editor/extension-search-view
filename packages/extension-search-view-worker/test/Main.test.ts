e7919572606110314ffeee903d2bfb57eebdcc9e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-26 23:05:57 +0100

    feature: add main test (#250)

--- packages/extension-search-view-worker/test/Main.test.ts
+++ packages/extension-search-view-worker/test/Main.test.ts
@@ -1,6 +1,20 @@
-import { expect, test } from '@jest/globals'
-import * as Main from '../src/parts/Main/Main.ts'
+import { expect, jest, test, beforeEach } from '@jest/globals'
 
-test('main', () => {
-  expect(typeof Main.main).toBe('function')
+beforeEach(() => {
+  jest.resetAllMocks()
+})
+
+const mockListen = jest.fn()
+
+jest.unstable_mockModule('../src/parts/Listen/Listen.ts', () => {
+  return {
+    listen: mockListen,
+  }
+})
+
+const Main = await import('../src/parts/Main/Main.ts')
+
+test('main', async () => {
+  await Main.main()
+  expect(mockListen).toHaveBeenCalled()
 })
