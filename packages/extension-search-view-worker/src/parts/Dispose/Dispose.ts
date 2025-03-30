021490240e8d038877eb57f98fdb06482fda09a9
Le Vivilet <levivlet@pm.me>
2025-03-30 16:39:04 +0200

    import

--- packages/extension-search-view-worker/src/parts/Dispose/Dispose.ts
+++ packages/extension-search-view-worker/src/parts/Dispose/Dispose.ts
@@ -1,4 +1,4 @@
-import * as ExtensionSearchViewStates from '../ExtensionSearchViewStates/ExtensionSearchViewStates.js'
+import * as ExtensionSearchViewStates from '../ExtensionSearchViewStates/ExtensionSearchViewStates.ts'
 
 export const dispose = (uid: number): void => {
   ExtensionSearchViewStates.remove(uid)
