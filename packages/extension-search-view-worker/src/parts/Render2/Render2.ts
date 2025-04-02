1ff9442390dac75901b3371fad9318e4b74b6143
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-02 18:59:35 +0200

    feature: remove unused code (#272)

--- packages/extension-search-view-worker/src/parts/Render2/Render2.ts
+++
@@ -1,7 +0,0 @@
-import * as ExtensionSearchViewStates from '../ExtensionSearchViewStates/ExtensionSearchViewStates.ts'
-import * as Render from '../Render/Render.ts'
-
-export const render2 = (uid: number): readonly any[] => {
-  const { oldState, newState } = ExtensionSearchViewStates.get(uid)
-  return Render.doRender(oldState, newState)
-}
