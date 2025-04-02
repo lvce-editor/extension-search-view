1bc9b8c7f830e6bdc2ca098dbf4f9f4b9ab123ab
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-02 19:00:14 +0200

    feature: add search value diff (#273)

--- packages/extension-search-view-worker/src/parts/DiffModules/DiffModules.ts
+++ packages/extension-search-view-worker/src/parts/DiffModules/DiffModules.ts
@@ -3,7 +3,15 @@ import * as DiffHeader from '../DiffHeader/DiffHeader.ts'
 import * as DiffItems from '../DiffItems/DiffItems.ts'
 import * as DiffMessage from '../DiffMessage/DiffMessage.ts'
 import * as DiffScrollBar from '../DiffScrollBar/DiffScrollBar.ts'
+import * as DiffSearchValue from '../DiffSearchValue/DiffSearchValue.ts'
 
-export const modules = [DiffScrollBar.isEqual, DiffMessage.isEqual, DiffHeader.isEqual, DiffItems.isEqual, DiffFocus.isEqual]
+export const modules = [DiffScrollBar.isEqual, DiffMessage.isEqual, DiffHeader.isEqual, DiffItems.isEqual, DiffFocus.isEqual, DiffSearchValue.isEqual]
 
-export const numbers = [DiffScrollBar.diffType, DiffMessage.diffType, DiffHeader.diffType, DiffItems.diffType, DiffFocus.diffType]
+export const numbers = [
+  DiffScrollBar.diffType,
+  DiffMessage.diffType,
+  DiffHeader.diffType,
+  DiffItems.diffType,
+  DiffFocus.diffType,
+  DiffSearchValue.diffType,
+]
