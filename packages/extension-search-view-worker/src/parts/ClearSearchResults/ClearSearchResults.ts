511d3b7c8106fe66afb69811e1d9192f1d7dc7ff
Le Vivilet <levivlet@pm.me>
2025-04-11 22:31:37 +0200

    remove log

--- packages/extension-search-view-worker/src/parts/ClearSearchResults/ClearSearchResults.ts
+++ packages/extension-search-view-worker/src/parts/ClearSearchResults/ClearSearchResults.ts
@@ -2,6 +2,5 @@ import type { State } from '../State/State.ts'
 import * as ViewletExtensionsHandleInput from '../HandleInput/HandleInput.ts'
 
 export const clearSearchResults = (state: State): Promise<State> => {
-  console.log('clear')
   return ViewletExtensionsHandleInput.handleInput(state, '')
 }
--- packages/extension-search-view-worker/src/parts/GetSearchFieldButtonVirtualDom/GetSearchFieldButtonVirtualDom.ts
+++ packages/extension-search-view-worker/src/parts/GetSearchFieldButtonVirtualDom/GetSearchFieldButtonVirtualDom.ts
@@ -1,6 +1,6 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
-import * as ClassNames from '../ClassNames/ClassNames.ts'
 import type { InputAction } from '../InputAction/InputAction.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
