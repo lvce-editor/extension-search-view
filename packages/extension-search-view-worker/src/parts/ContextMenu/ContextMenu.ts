4d47bc37b7dab5227abd40a43e31a9a9d50eb742
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-11 22:20:52 +0200

    feature: open context menu (#285)
    * feature: open context menu
    
    * test
    
    * test

--- packages/extension-search-view-worker/src/parts/ContextMenu/ContextMenu.ts
+++ packages/extension-search-view-worker/src/parts/ContextMenu/ContextMenu.ts
@@ -1,8 +1,9 @@
 import * as Assert from '../Assert/Assert.ts'
+import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
 
 export const show = async (x: number, y: number, id: number): Promise<void> => {
   Assert.number(x)
   Assert.number(y)
   Assert.number(id)
-  // TODO
+  await ParentRpc.invoke('ContextMenu.show', x, y, id)
 }
