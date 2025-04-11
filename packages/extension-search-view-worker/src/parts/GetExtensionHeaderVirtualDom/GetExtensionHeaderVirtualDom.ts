bd2191cc47d807f92dc4f8140594299adf8249a8
Le Vivilet <levivlet@pm.me>
2025-04-11 22:21:44 +0200

    lint

--- packages/extension-search-view-worker/src/parts/GetExtensionHeaderVirtualDom/GetExtensionHeaderVirtualDom.ts
+++ packages/extension-search-view-worker/src/parts/GetExtensionHeaderVirtualDom/GetExtensionHeaderVirtualDom.ts
@@ -1,8 +1,8 @@
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as GetSearchFieldVirtualDom from '../GetSearchFieldVirtualDom/GetSearchFieldVirtualDom.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
-import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 
 export const getExtensionHeaderVirtualDom = (placeholder: string, actions: readonly any[]): readonly VirtualDomNode[] => {
   return [
