a116ba078ca8b43285daa3afb91756c118f502c0
Le Vivilet <levivlet@pm.me>
2025-04-24 21:52:00 +0200

    lint

--- packages/extension-search-view-worker/src/parts/ExtensionSearchViewStates/ExtensionSearchViewStates.ts
+++ packages/extension-search-view-worker/src/parts/ExtensionSearchViewStates/ExtensionSearchViewStates.ts
@@ -1,4 +1,4 @@
-import type { State } from '../State/State.ts'
 import * as ViewletRegistry from '@lvce-editor/viewlet-registry'
+import type { State } from '../State/State.ts'
 
 export const { get, set, dispose, getKeys, wrapCommand } = ViewletRegistry.create<State>()
