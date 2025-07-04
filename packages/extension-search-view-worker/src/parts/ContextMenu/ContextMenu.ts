bc89d6f9853bd3c699b8e08210b8efc4b5a2fe10
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-04 12:38:18 +0200

    feature: rename module (#314)

--- packages/extension-search-view-worker/src/parts/ContextMenu/ContextMenu.ts
+++ packages/extension-search-view-worker/src/parts/ContextMenu/ContextMenu.ts
@@ -1,9 +1,9 @@
 import * as Assert from '../Assert/Assert.ts'
-import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
+import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 export const show = async (x: number, y: number, id: number): Promise<void> => {
   Assert.number(x)
   Assert.number(y)
   Assert.number(id)
-  await ParentRpc.invoke('ContextMenu.show', x, y, id)
+  await RendererWorker.invoke('ContextMenu.show', x, y, id)
 }
--- packages/extension-search-view-worker/test/SetFocus.test.ts
+++ packages/extension-search-view-worker/test/SetFocus.test.ts
@@ -1,12 +1,12 @@
 import { expect, jest, test } from '@jest/globals'
-import * as ParentRpc from '../src/parts/ParentRpc/ParentRpc.ts'
+import * as RendererWorker from '../src/parts/RendererWorker/RendererWorker.ts'
 import { setFocus } from '../src/parts/SetFocus/SetFocus.ts'
 
 test('setFocus calls RPC with correct parameters', async () => {
   const mockRpc = {
     invoke: jest.fn(),
   } as any
-  ParentRpc.set(mockRpc)
+  RendererWorker.set(mockRpc)
   const focusId = 123
   await setFocus(focusId)
   expect(mockRpc.invoke).toHaveBeenCalledWith('Focus.setFocus', focusId)
