dc064724ad564dbc20f68c2adaf2ee8a97188782
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-14 18:13:18 +0200

    feature: display message if available (#332)
    * feature: display message if available
    
    * lint
    
    * lint

--- packages/extension-search-view-worker/src/parts/GetExtensionsViewVirtualDom/GetExtensionsViewVirtualDom.ts
+++ packages/extension-search-view-worker/src/parts/GetExtensionsViewVirtualDom/GetExtensionsViewVirtualDom.ts
@@ -1,11 +1,26 @@
-import { AriaRoles, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import { AriaRoles, text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { State } from '../State/State.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import type { VisibleItem } from '../VisibleItem/VisibleItem.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import { getExtensionHeaderVirtualDom } from '../GetExtensionHeaderVirtualDom/GetExtensionHeaderVirtualDom.ts'
 import * as GetExtensionsVirtualDom from '../GetExtensionsVirtualDom/GetExtensionsVirtualDom.ts'
 import * as GetVisibleExtensions from '../GetVisibleExtensions/GetVisibleExtensions.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
+
+const getContentVirtualDom = (visibleExtensions: readonly VisibleItem[], message: string): readonly VirtualDomNode[] => {
+  if (message) {
+    return [{ type: VirtualDomElements.Div, childCount: 1 }, text(message)]
+  }
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className: MergeClassNames.mergeClassNames(ClassNames.Viewlet, ClassNames.List),
+      childCount: 2,
+    },
+    ...GetExtensionsVirtualDom.getExtensionsVirtualDom(visibleExtensions),
+  ]
+}
 
 export const getExtensionsViewVirtualDom = (state: State): readonly VirtualDomNode[] => {
   const visibleExtensions = GetVisibleExtensions.getVisible(state)
@@ -19,11 +34,6 @@ export const getExtensionsViewVirtualDom = (state: State): readonly VirtualDomNo
       role: AriaRoles.None,
     },
     ...getExtensionHeaderVirtualDom(state.placeholder, []),
-    {
-      type: VirtualDomElements.Div,
-      className: MergeClassNames.mergeClassNames(ClassNames.Viewlet, ClassNames.List),
-      childCount: 2,
-    },
-    ...GetExtensionsVirtualDom.getExtensionsVirtualDom(visibleExtensions),
+    ...getContentVirtualDom(visibleExtensions, state.message),
   ]
 }
