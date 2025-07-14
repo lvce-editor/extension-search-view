e2dca7e1818d2f568692674089518f1e3ffcb08e
Le Vivilet <levivlet@pm.me>
2025-07-14 17:58:39 +0200

    lint

--- packages/extension-search-view-worker/src/parts/GetExtensionsViewVirtualDom/GetExtensionsViewVirtualDom.ts
+++ packages/extension-search-view-worker/src/parts/GetExtensionsViewVirtualDom/GetExtensionsViewVirtualDom.ts
@@ -1,11 +1,11 @@
 import { AriaRoles, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { State } from '../State/State.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
 import { getExtensionHeaderVirtualDom } from '../GetExtensionHeaderVirtualDom/GetExtensionHeaderVirtualDom.ts'
 import * as GetExtensionsVirtualDom from '../GetExtensionsVirtualDom/GetExtensionsVirtualDom.ts'
 import * as GetVisibleExtensions from '../GetVisibleExtensions/GetVisibleExtensions.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
-import * as ClassNames from '../ClassNames/ClassNames.ts'
 
 export const getExtensionsViewVirtualDom = (state: State): readonly VirtualDomNode[] => {
   const visibleExtensions = GetVisibleExtensions.getVisible(state)
