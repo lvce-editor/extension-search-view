18454b4542371b98d4ca01db2455232a98bafa34
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-14 18:00:09 +0200

    feature: remove unused code (#326)

--- packages/extension-search-view-worker/src/parts/RendererWorkerApi/RendererWorkerApi.ts
+++
@@ -1,6 +0,0 @@
-export interface RendererWorkerApi {
-  readonly 'Main.openUri': (uri: string) => Promise<void>
-  readonly 'Focus.setFocus': (focusId: number) => Promise<string>
-  readonly 'ExtensionManagement.getAllExtensions': () => Promise<readonly any[]>
-  readonly 'ContextMenu.show': (x: number, y: number, id: number) => Promise<void>
-}
