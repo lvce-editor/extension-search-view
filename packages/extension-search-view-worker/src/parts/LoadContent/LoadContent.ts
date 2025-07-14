68202866ff4c77d07e1177afff173160a622c2f8
Le Vivilet <levivlet@pm.me>
2025-07-14 18:00:24 +0200

    feature: wrap loadContent command

--- packages/extension-search-view-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/extension-search-view-worker/src/parts/LoadContent/LoadContent.ts
@@ -1,4 +1,4 @@
-import * as ExtensionSearchViewStates from '../ExtensionSearchViewStates/ExtensionSearchViewStates.ts'
+import type { State } from '../State/State.ts'
 import * as GetAllExtensions from '../GetAllExtensions/GetAllExtensions.ts'
 import * as GetViewletSize from '../GetViewletSize/GetViewletSize.ts'
 import * as HandleInput from '../HandleInput/HandleInput.ts'
@@ -6,14 +6,13 @@ import * as InputSource from '../InputSource/InputSource.ts'
 import * as NormalizeExtensions from '../NormalizeExtensions/NormalizeExtensions.ts'
 import * as RestoreState from '../RestoreState/RestoreState.ts'
 
-export const loadContent = async (uid: number, savedState: unknown): Promise<void> => {
-  const { newState } = ExtensionSearchViewStates.get(uid)
-  const { width, platform, assetDir } = newState
+export const loadContent = async (state: State, savedState: unknown): Promise<State> => {
+  const { width, platform, assetDir } = state
   const { searchValue } = RestoreState.restoreState(savedState)
   // TODO just get local extensions on demand (not when query string is already different)
   const allExtensions = await GetAllExtensions.getAllExtensions(platform)
   const size = GetViewletSize.getViewletSize(width)
   const normalized = NormalizeExtensions.normalizeExtension(allExtensions, platform, assetDir)
-  const updatedState = await HandleInput.handleInput({ ...newState, allExtensions: normalized, size, inputSource: InputSource.Script }, searchValue)
-  ExtensionSearchViewStates.set(uid, newState, updatedState)
+  const updatedState = await HandleInput.handleInput({ ...state, allExtensions: normalized, size, inputSource: InputSource.Script }, searchValue)
+  return updatedState
 }
