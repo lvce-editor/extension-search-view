223a2e3e07a20d499f302ea762f0b39dbfa2df31
Le Vivilet <levivlet@pm.me>
2025-07-14 17:58:20 +0200

    feature: use classnames enum

--- packages/extension-search-view-worker/src/parts/GetExtensionsViewVirtualDom/GetExtensionsViewVirtualDom.ts
+++ packages/extension-search-view-worker/src/parts/GetExtensionsViewVirtualDom/GetExtensionsViewVirtualDom.ts
@@ -1,25 +1,27 @@
-import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import { AriaRoles, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { State } from '../State/State.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import { getExtensionHeaderVirtualDom } from '../GetExtensionHeaderVirtualDom/GetExtensionHeaderVirtualDom.ts'
 import * as GetExtensionsVirtualDom from '../GetExtensionsVirtualDom/GetExtensionsVirtualDom.ts'
 import * as GetVisibleExtensions from '../GetVisibleExtensions/GetVisibleExtensions.ts'
+import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
 
 export const getExtensionsViewVirtualDom = (state: State): readonly VirtualDomNode[] => {
   const visibleExtensions = GetVisibleExtensions.getVisible(state)
   return [
     {
       type: VirtualDomElements.Div,
-      className: 'Viewlet Extensions',
+      className: MergeClassNames.mergeClassNames(ClassNames.Viewlet, ClassNames.Extensions),
       childCount: 2,
       ariaLive: 'polite',
       ariaBusy: false,
-      role: 'none',
+      role: AriaRoles.None,
     },
     ...getExtensionHeaderVirtualDom(state.placeholder, []),
     {
       type: VirtualDomElements.Div,
-      className: 'Viewlet List',
+      className: MergeClassNames.mergeClassNames(ClassNames.Viewlet, ClassNames.List),
       childCount: 2,
     },
     ...GetExtensionsVirtualDom.getExtensionsVirtualDom(visibleExtensions),
