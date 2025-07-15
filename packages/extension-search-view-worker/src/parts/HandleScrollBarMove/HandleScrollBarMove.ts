3f46296585f27bf74103e7245b4a25d3222701d7
Le Vivilet <levivlet@pm.me>
2025-07-15 19:49:49 +0200

    tsc

--- packages/extension-search-view-worker/src/parts/HandleScrollBarMove/HandleScrollBarMove.ts
+++ packages/extension-search-view-worker/src/parts/HandleScrollBarMove/HandleScrollBarMove.ts
@@ -1,6 +1,5 @@
-import type { State } from '../State/State.ts'
-import type { State } from '../State/State.ts'
 import { setDeltaY } from '../SetDeltaY/SetDeltaY.ts'
+import type { State } from '../State/State.ts'
 
 const getNewPercent = (contentHeight: number, scrollBarHeight: number, relativeY: number): number => {
   if (relativeY <= contentHeight - scrollBarHeight / 2) {
@@ -11,7 +10,7 @@ const getNewPercent = (contentHeight: number, scrollBarHeight: number, relativeY
   return 1
 }
 
-export const handleScrollBarMove = (state: State, eventY: number): any => {
+export const handleScrollBarMove = (state: State, eventY: number): State => {
   const { y, headerHeight, handleOffset, finalDeltaY, height, scrollBarHeight } = state
   const relativeY = eventY - y - headerHeight - handleOffset
   const contentHeight = height - headerHeight
