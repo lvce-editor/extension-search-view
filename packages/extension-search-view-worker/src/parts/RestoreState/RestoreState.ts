d01d4468869470cbd8dfbe84fc8c24713f42cd84
Le Vivilet <levivlet@pm.me>
2025-07-15 15:13:18 +0200

    lint

--- packages/extension-search-view-worker/src/parts/RestoreState/RestoreState.ts
+++ packages/extension-search-view-worker/src/parts/RestoreState/RestoreState.ts
@@ -1,5 +1,5 @@
-import { getSavedDeltaY, getSavedValue } from '../GetSavedValue/GetSavedValue.ts'
 import type { RestoredState } from '../RestoredState/RestoredState.ts'
+import { getSavedDeltaY, getSavedValue } from '../GetSavedValue/GetSavedValue.ts'
 
 export const restoreState = (savedState: unknown): RestoredState => {
   const searchValue = getSavedValue(savedState)
