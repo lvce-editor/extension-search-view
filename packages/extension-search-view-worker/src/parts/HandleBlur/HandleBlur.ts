d0f2c03dc71fb8b17080e7a2a4618d8d94e94549
Le Vivilet <levivlet@pm.me>
2025-07-15 14:56:32 +0200

    feature: on blur, clear focus

--- packages/extension-search-view-worker/src/parts/HandleBlur/HandleBlur.ts
+++ packages/extension-search-view-worker/src/parts/HandleBlur/HandleBlur.ts
@@ -1,5 +1,9 @@
 import type { State } from '../State/State.ts'
+import * as FocusId from '../FocusId/FocusId.ts'
 
 export const handleBlur = (state: State): State => {
-  return state
+  return {
+    ...state,
+    focus: FocusId.None,
+  }
 }
