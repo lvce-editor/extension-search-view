e117adb0e4a021caf313a9ed97c527357a4ce857
Le Vivilet <levivlet@pm.me>
2025-07-15 19:51:09 +0200

    test

--- packages/extension-search-view-worker/test/GetRenderer.test.ts
+++ packages/extension-search-view-worker/test/GetRenderer.test.ts
@@ -1,12 +1,12 @@
 import { expect, test } from '@jest/globals'
 import * as DiffType from '../src/parts/DiffType/DiffType.ts'
 import * as GetRenderer from '../src/parts/GetRenderer/GetRenderer.ts'
-import * as RenderItems from '../src/parts/RenderItems/RenderItems.ts'
+import { renderItems2 } from '../src/parts/RenderItems2/RenderItems2.ts'
 import * as RenderMessage from '../src/parts/RenderMessage/RenderMessage.ts'
 
 test('returns renderItems renderer for RenderItems diff type', () => {
   const renderer = GetRenderer.getRenderer(DiffType.RenderItems)
-  expect(renderer).toBe(RenderItems.renderItems)
+  expect(renderer).toBe(renderItems2)
 })
 
 test('returns renderMessage renderer for RenderMessage diff type', () => {
