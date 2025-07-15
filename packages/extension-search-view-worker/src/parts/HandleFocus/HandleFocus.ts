0b98c2e02d82801aef35f3e68610fc043be9df58
Le Vivilet <levivlet@pm.me>
2025-07-15 10:36:05 +0200

    update blur function

--- packages/extension-search-view-worker/src/parts/HandleFocus/HandleFocus.ts
+++ packages/extension-search-view-worker/src/parts/HandleFocus/HandleFocus.ts
@@ -2,7 +2,6 @@ import * as FocusId from '../FocusId/FocusId.ts'
 import type { State } from '../State/State.ts'
 
 export const handleFocus = async (state: State): Promise<State> => {
-  console.log('got focus')
   return {
     ...state,
     focus: FocusId.List,
