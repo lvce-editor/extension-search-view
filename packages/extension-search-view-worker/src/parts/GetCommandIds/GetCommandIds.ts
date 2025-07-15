ad16c3ad339aa50c43ffddc0ee698df2bc4fa6aa
Le Vivilet <levivlet@pm.me>
2025-07-15 15:12:41 +0200

    simplify code

--- packages/extension-search-view-worker/src/parts/GetCommandIds/GetCommandIds.ts
+++ packages/extension-search-view-worker/src/parts/GetCommandIds/GetCommandIds.ts
@@ -1,5 +1,12 @@
-import { commandIds } from '../CommandIds/CommandIds.ts'
+import { commandMapRef } from '../CommandMapRef/CommandMapRef.ts'
+
+const toCommandId = (key: string) => {
+  const dotIndex = key.indexOf('.')
+  return key.slice(dotIndex + 1)
+}
 
 export const getCommandIds = (): readonly string[] => {
-  return commandIds
+  const keys = Object.keys(commandMapRef)
+  const ids = keys.map(toCommandId)
+  return ids
 }
