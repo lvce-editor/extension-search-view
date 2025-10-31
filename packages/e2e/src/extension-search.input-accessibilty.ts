f72810a5b36e5745d8f7860da04195623170f5e0
Le Vivilet <levivilet@pm.me>
2025-10-31 19:00:25 +0100

    feature: add input accessibility e2e test (#418)

---
+++ packages/e2e/src/extension-search.input-accessibilty.ts
@@ -0,0 +1,17 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const test: Test = async ({ Locator, expect, SideBar }) => {
+  // act
+  await SideBar.open('Extensions')
+  const extensionsView = Locator('.Extensions')
+  await expect(extensionsView).toBeVisible()
+
+  // assert
+  const input = extensionsView.locator('.MultilineInputBox')
+  await expect(input).toBeVisible()
+  await expect(input).toHaveAttribute('type', 'search')
+  await expect(input).toHaveAttribute('spellcheck', 'false')
+  await expect(input).toHaveAttribute('autocapitalize', 'off')
+  await expect(input).toHaveAttribute('placeholder', 'Search Extensions in Marketplace')
+  await expect(input).toHaveAttribute('name', 'extensions')
+}
