ff3a68251a29118187ccf3b8216661294cd2dfc0
Le Vivilet <levivilet@pm.me>
2025-10-31 18:24:08 +0100

    feature: add context menu e2e test (#401)

--- packages/e2e/src/extension-search.context-menu.ts
+++ packages/e2e/src/extension-search.context-menu.ts
@@ -5,12 +5,23 @@ export const test: Test = async ({ Locator, expect, SideBar, Command }) => {
   await SideBar.open('Extensions')
   const extensionsView = Locator('.Extensions')
   await expect(extensionsView).toBeVisible()
-
-  // act
   await Command.execute('Extensions.handleInput', 'atom', 2)
 
+  // act
+  await Command.execute('Extensions.handleContextMenu', 0, 0, 0)
+
   // assert
-  const listItems = Locator('.Extensions .ListItems')
-  await expect(listItems).toHaveCount(1)
-  const firstItem = listItems.locator('.ExtensionListItem')
+  const contextMenu = Locator('.Menu')
+  await expect(contextMenu).toBeVisible()
+  const menuItems = contextMenu.locator('.MenuItem')
+  const menuItem1 = menuItems.nth(0)
+  await expect(menuItem1).toHaveText('Enable')
+  const menuItem2 = menuItems.nth(1)
+  await expect(menuItem2).toHaveText('Disable')
+  const menuItem3 = menuItems.nth(2)
+  await expect(menuItem3).toHaveText('Install Another Version')
+  const menuItem4 = menuItems.nth(3)
+  await expect(menuItem4).toHaveText('Copy')
+  const menuItem5 = menuItems.nth(4)
+  await expect(menuItem5).toHaveText('CopyExtensionId')
 }
