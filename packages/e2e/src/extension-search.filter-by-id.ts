90c25789a3c0dbca2af38d018dbe2be62e843cb8
Le Vivilet <levivilet@pm.me>
2025-10-31 18:40:58 +0100

    feature: enable id filter e2e test

--- packages/e2e/src/extension-search.filter-by-id.ts
+++ packages/e2e/src/extension-search.filter-by-id.ts
@@ -1,6 +1,6 @@
 import type { Test } from '@lvce-editor/test-with-playwright'
 
-export const skip = 1
+// export const skip = 1
 
 export const test: Test = async ({ Locator, expect, SideBar, Command }) => {
   // arrange
