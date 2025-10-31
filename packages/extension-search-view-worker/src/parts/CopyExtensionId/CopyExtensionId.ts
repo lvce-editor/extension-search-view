78d2253ac764f1ca366b1b8628e7da7a515c27f6
Le Vivilet <levivilet@pm.me>
2025-10-31 18:57:02 +0100

    feature: enable clipboard e2e test (#417)

--- packages/extension-search-view-worker/src/parts/CopyExtensionId/CopyExtensionId.ts
+++ packages/extension-search-view-worker/src/parts/CopyExtensionId/CopyExtensionId.ts
@@ -1,6 +1,13 @@
+import { RendererWorker } from '@lvce-editor/rpc-registry'
 import type { State } from '../State/State.ts'
 
 export const copyExtensionId = async (state: State): Promise<State> => {
-  // TODO
+  const { items, focusedIndex } = state
+  if (focusedIndex === -1) {
+    return state
+  }
+  const item = items[focusedIndex]
+  const { id } = item
+  await RendererWorker.writeClipBoardText(id)
   return state
 }
