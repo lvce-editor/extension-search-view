0daa91052265686737c9ed44b1c0b85e1502d1f2
Le Vivilet <levivilet@pm.me>
2025-10-31 18:35:41 +0100

    feature: add no extensions found e2e test (#404)

---
+++ packages/e2e/src/extension-search.no-results.ts
@@ -0,0 +1,18 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const test: Test = async ({ Locator, expect, SideBar, Command }) => {
+  // arrange
+  await SideBar.open('Extensions')
+  const extensionsView = Locator('.Extensions')
+  await expect(extensionsView).toBeVisible()
+
+  // act
+  await Command.execute('Extensions.handleInput', 'not-found', 2)
+
+  // assert
+  const listItems = Locator('.Extensions .ListItems')
+  await expect(listItems).toHaveCount(0)
+  const noExtensionsFoundMessage = Locator('.Extensions > div:nth-of-type(2)')
+  await expect(noExtensionsFoundMessage).toBeVisible()
+  await expect(noExtensionsFoundMessage).toHaveText('No extensions found.')
+}
