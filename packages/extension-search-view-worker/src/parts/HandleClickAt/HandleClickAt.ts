f00a0331f9f6ae935c5c680066718c4deb183f85
Le Vivilet <levivilet@pm.me>
2025-10-31 18:24:52 +0100

    feature: don't open detail view on rightclick (#402)

--- packages/extension-search-view-worker/src/parts/HandleClickAt/HandleClickAt.ts
+++ packages/extension-search-view-worker/src/parts/HandleClickAt/HandleClickAt.ts
@@ -1,8 +1,12 @@
+import { MouseEventType } from '@lvce-editor/virtual-dom-worker'
 import type { State } from '../State/State.ts'
 import * as GetListIndex from '../GetListIndex/GetListIndex.ts'
 import { handleClick } from '../HandleClick/HandleClick.ts'
 
-export const handleClickAt = async (state: State, eventX: number, eventY: number): Promise<State> => {
+export const handleClickAt = async (state: State, button: number, eventX: number, eventY: number): Promise<State> => {
+  if (button !== MouseEventType.LeftClick) {
+    return state
+  }
   const { x, y, itemHeight, deltaY, headerHeight } = state
   const index = GetListIndex.getListIndex(eventX, eventY, x, y, deltaY, itemHeight, headerHeight)
   return handleClick(state, index)
