9fca90d1d186104f0e9dd728f30612f261d2cad6
Le Vivilet <levivilet@pm.me>
2025-10-31 17:29:28 +0100

    feature: add one result e2e test

---
+++ packages/e2e/src/extension-search.one-result.ts
@@ -0,0 +1,16 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const test: Test = async ({ Locator, expect, SideBar, Command }) => {
+  // arrange
+  await SideBar.open('Extensions')
+  const extensionsView = Locator('.Extensions')
+  await expect(extensionsView).toBeVisible()
+
+  // act
+  await Command.execute('Extensions.handleInput', 'atom', 2)
+
+  // assert
+  const listItems = Locator('.Extensions .ListItems')
+  await expect(listItems).toHaveCount(1)
+  const firstItem = listItems.locator('.ExtensionListItem')
+}
