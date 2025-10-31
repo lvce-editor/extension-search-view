39780e9c89d32e0a60869c07944cfd82544fc0bc
Le Vivilet <levivilet@pm.me>
2025-10-31 19:12:50 +0100

    feature: implement text copy function (#421)

--- packages/extension-search-view-worker/src/parts/CopyExtensionInfo/CopyExtensionInfo.ts
+++ packages/extension-search-view-worker/src/parts/CopyExtensionInfo/CopyExtensionInfo.ts
@@ -1,6 +1,14 @@
+import { RendererWorker } from '@lvce-editor/rpc-registry'
 import type { State } from '../State/State.ts'
+import { getExtensionInfoText } from '../GetExtensionInfoText/GetExtensionInfoText.ts'
 
 export const copyExtensionInfo = async (state: State): Promise<State> => {
-  // TODO
+  const { items, focusedIndex } = state
+  if (focusedIndex === -1) {
+    return state
+  }
+  const item = items[focusedIndex]
+  const text = getExtensionInfoText(item)
+  await RendererWorker.writeClipBoardText(text)
   return state
 }
