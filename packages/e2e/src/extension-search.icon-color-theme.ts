408f9543c27a75cc85a506c09739d21502b2610d
Le Vivilet <levivilet@pm.me>
2025-10-31 18:29:35 +0100

    feature: add color theme e2e test

---
+++ packages/e2e/src/extension-search.icon-color-theme.ts
@@ -0,0 +1,19 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const test: Test = async ({ Locator, expect, SideBar, Command }) => {
+  // arrange
+  await SideBar.open('Extensions')
+  const extensionsView = Locator('.Extensions')
+  await expect(extensionsView).toBeVisible()
+
+  // act
+  await Command.execute('Extensions.handleInput', 'atom', 2)
+
+  // assert
+  const listItems = Locator('.Extensions .ListItems')
+  await expect(listItems).toHaveCount(1)
+  const firstItem = listItems.locator('.ExtensionListItem')
+  const firstItemIcon = firstItem.locator('.ExtensionListItemIcon')
+  await expect(firstItemIcon).toBeVisible()
+  // TODO verify that icon src is as expected
+}
