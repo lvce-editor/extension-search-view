045be085ac8129f1eec3da87e2d37b9840a6fd92
Le Vivilet <levivilet@pm.me>
2025-11-01 08:30:58 +0100

    feature: query extensions from extension host worker (#430)

--- packages/extension-search-view-worker/src/parts/ExtensionManagement/ExtensionManagement.ts
+++ packages/extension-search-view-worker/src/parts/ExtensionManagement/ExtensionManagement.ts
@@ -1,9 +1,15 @@
+import * as ExtensionHostWorker from '../ExtensionHostWorker/ExtensionHostWorker.ts'
 import * as PlatformType from '../PlatformType/PlatformType.ts'
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 export const getAllExtensions = async (platform: number): Promise<readonly any[]> => {
   if (platform === PlatformType.Web) {
-    return []
+    try {
+      const extensions = await ExtensionHostWorker.invoke('Extensions.getExtensions')
+      return extensions
+    } catch {
+      return []
+    }
   }
   // @ts-ignore todo
   return RendererWorker.invoke('ExtensionManagement.getAllExtensions')
