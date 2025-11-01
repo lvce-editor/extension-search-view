95108e2fdff08f4ae1e92e2e173675462dca0466
Le Vivilet <levivilet@pm.me>
2025-11-01 08:22:10 +0100

    test

--- packages/extension-search-view-worker/test/GetCommandIds.test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from '@jest/globals'
-import * as GetCommandIds from '../src/parts/GetCommandIds/GetCommandIds.ts'
-
-test('returns the commandIds array', () => {
-  expect(GetCommandIds.getCommandIds()).toBeDefined()
-})
