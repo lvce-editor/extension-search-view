1d4158fbb809c03eecaef6899269137f5e09b028
Le Vivilet <levivilet@pm.me>
2025-11-03 23:41:30 +0100

    feature: when clearing results, set focus to input (#468)

--- packages/extension-search-view-worker/src/parts/ClearSearchResults/ClearSearchResults.ts
+++ packages/extension-search-view-worker/src/parts/ClearSearchResults/ClearSearchResults.ts
@@ -1,6 +1,15 @@
+import { InputSource } from '@lvce-editor/constants'
 import type { State } from '../State/State.ts'
+import * as FocusId from '../FocusId/FocusId.ts'
 import * as ViewletExtensionsHandleInput from '../HandleInput/HandleInput.ts'
 
 export const clearSearchResults = (state: State): Promise<State> => {
-  return ViewletExtensionsHandleInput.handleInput(state, '')
+  return ViewletExtensionsHandleInput.handleInput(
+    {
+      ...state,
+      focus: FocusId.Input,
+    },
+    '',
+    InputSource.Script,
+  )
 }
