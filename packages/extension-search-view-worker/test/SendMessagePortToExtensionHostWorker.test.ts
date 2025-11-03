16b65f6b803f56cdd9ba8b280b9982fb21fefccf
Le Vivilet <levivilet@pm.me>
2025-11-03 22:44:53 +0100

    fix: tsc

--- packages/extension-search-view-worker/test/SendMessagePortToExtensionHostWorker.test.ts
+++ packages/extension-search-view-worker/test/SendMessagePortToExtensionHostWorker.test.ts
@@ -2,12 +2,15 @@ import { expect, test } from '@jest/globals'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 import { sendMessagePortToExtensionHostWorker } from '../src/parts/SendMessagePortToExtensionHostWorker/SendMessagePortToExtensionHostWorker.ts'
 
+interface MessagePort {
+  postMessage(message: unknown): void
+}
+
 test('sendMessagePortToExtensionHostWorker calls RPC with correct parameters', async () => {
   const mockRpc = RendererWorker.registerMockRpc({
     'SendMessagePortToExtensionHostWorker.sendMessagePortToExtensionHostWorker'() {},
   })
-  // @ts-ignore
-  const port = { postMessage: (): void => {} }
+  const port: MessagePort = { postMessage: (): void => {} }
   await sendMessagePortToExtensionHostWorker(port)
   expect(mockRpc.invocations).toEqual([
     ['SendMessagePortToExtensionHostWorker.sendMessagePortToExtensionHostWorker', port, 'HandleMessagePort.handleMessagePort2', 0],
