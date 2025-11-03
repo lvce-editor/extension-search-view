1f2ab2c7da73f789d1aed04c2736d76fd404bb5b
Le Vivilet <levivilet@pm.me>
2025-11-03 23:25:39 +0100

    feature: simplify diff

--- packages/extension-search-view-worker/src/parts/Diff2/Diff2.ts
+++ packages/extension-search-view-worker/src/parts/Diff2/Diff2.ts
@@ -2,13 +2,5 @@ import * as DiffModules from '../DiffModules/DiffModules.ts'
 import * as ExtensionSearchViewStates from '../ExtensionSearchViewStates/ExtensionSearchViewStates.ts'
 
 export const diff2 = (uid: number): readonly number[] => {
-  const { oldState, newState } = ExtensionSearchViewStates.get(uid)
-  const diffResult: number[] = []
-  for (let i = 0; i < DiffModules.modules.length; i++) {
-    const fn = DiffModules.modules[i]
-    if (!fn(oldState, newState)) {
-      diffResult.push(DiffModules.numbers[i])
-    }
-  }
-  return diffResult
+  return ExtensionSearchViewStates.diff(uid, DiffModules.modules, DiffModules.numbers)
 }
