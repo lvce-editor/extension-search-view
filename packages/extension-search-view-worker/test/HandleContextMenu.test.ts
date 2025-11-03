cd5c44625ec72c4a6345288165038d3e40ca393d
Le Vivilet <levivilet@pm.me>
2025-11-03 22:26:08 +0100

    feature: add context menu test (#449)

--- packages/extension-search-view-worker/test/HandleContextMenu.test.ts
+++ packages/extension-search-view-worker/test/HandleContextMenu.test.ts
@@ -1,9 +1,15 @@
 import { expect, test } from '@jest/globals'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
 import * as CreateDefaultState from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import * as HandleContextMenu from '../src/parts/HandleContextMenu/HandleContextMenu.ts'
+import * as MenuEntryId from '../src/parts/MenuEntryId/MenuEntryId.ts'
 
-test.skip('handleContextMenu returns state', async () => {
+test('handleContextMenu returns state', async () => {
+  const mockRpc = RendererWorker.registerMockRpc({
+    'ContextMenu.show'() {},
+  })
   const state = CreateDefaultState.createDefaultState()
   const result = await HandleContextMenu.handleContextMenu(state, 0, 100, 200)
   expect(result).toBe(state)
+  expect(mockRpc.invocations).toEqual([['ContextMenu.show', 100, 200, MenuEntryId.ManageExtension]])
 })
