b3ee7db86cea01ad06772290ac4d045fe7130d88
Le Vivilet <levivilet@pm.me>
2025-11-03 22:14:04 +0100

    feature: add searchValue test

---
+++ packages/extension-search-view-worker/test/RenderSearchValue.test.ts
@@ -0,0 +1,22 @@
+import { expect, test } from '@jest/globals'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import * as InputName from '../src/parts/InputName/InputName.ts'
+import * as RenderSearchValue from '../src/parts/RenderSearchValue/RenderSearchValue.ts'
+
+test('returns method call array with empty search value', () => {
+  const state = createDefaultState()
+  const result = RenderSearchValue.renderSearchValue(state)
+  expect(result).toEqual(['Viewlet.setValueByName', state.uid, InputName.Extensions, state.searchValue])
+})
+
+test('returns method call array with search value', () => {
+  const state = { ...createDefaultState(), searchValue: 'test query' }
+  const result = RenderSearchValue.renderSearchValue(state)
+  expect(result).toEqual(['Viewlet.setValueByName', state.uid, InputName.Extensions, 'test query'])
+})
+
+test('returns method call array with different uid', () => {
+  const state = { ...createDefaultState(), uid: 123, searchValue: 'search' }
+  const result = RenderSearchValue.renderSearchValue(state)
+  expect(result).toEqual(['Viewlet.setValueByName', 123, InputName.Extensions, 'search'])
+})
