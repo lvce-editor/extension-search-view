87850ab688687537a81422fb3cea769f0c65dae1
Le Vivilet <levivilet@pm.me>
2025-11-03 22:23:24 +0100

    feature: add context menu tests (#443)

--- packages/extension-search-view-worker/test/ContextMenu.test.ts
+++ packages/extension-search-view-worker/test/ContextMenu.test.ts
@@ -1,7 +1,15 @@
 import { expect, test } from '@jest/globals'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
 import * as ContextMenu from '../src/parts/ContextMenu/ContextMenu.ts'
 
-test.skip('show function validates input parameters', async () => {
+test('show function validates input parameters', async () => {
+  const mockRpc = RendererWorker.registerMockRpc({
+    'ContextMenu.show'() {},
+  })
   await expect(ContextMenu.show(100, 200, 1)).resolves.not.toThrow()
   await expect(ContextMenu.show(0, 0, 0)).resolves.not.toThrow()
+  expect(mockRpc.invocations).toEqual([
+    ['ContextMenu.show', 100, 200, 1],
+    ['ContextMenu.show', 0, 0, 0],
+  ])
 })
