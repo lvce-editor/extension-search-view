1137092d527e8bb15edad686ac739318ae311403
Le Vivilet <levivilet@pm.me>
2025-11-09 20:25:31 +0100

    move

--- packages/extension-search-view-worker/src/parts/Resize/Resize.ts
+++ packages/extension-search-view-worker/src/parts/Resize/Resize.ts
@@ -1,16 +1,10 @@
+import type { Dimensions } from '../Dimensions/Dimensions.ts'
 import type { State } from '../State/State.ts'
 import * as GetFinalDeltaY from '../GetFinalDeltaY/GetFinalDeltaY.ts'
 import * as GetListHeight from '../GetListHeight/GetListHeight.ts'
 import * as GetNumberOfVisibleItems from '../GetNumberOfVisibleItems/GetNumberOfVisibleItems.ts'
 import * as GetScrollBarSize from '../GetScrollBarSize/GetScrollBarSize.ts'
 import * as ScrollBarFunctions from '../ScrollBarFunctions/ScrollBarFunctions.ts'
-
-interface Dimensions {
-  readonly x: number
-  readonly y: number
-  readonly width: number
-  readonly height: number
-}
 
 export const resize = async (state: State, dimensions: Dimensions): Promise<State> => {
   const { items, itemHeight, headerHeight, minimumSliderSize } = state
