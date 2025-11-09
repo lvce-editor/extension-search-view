0f4d169d2623161ecc1b3f4d10dc913e7a82b097
Le Vivilet <levivilet@pm.me>
2025-11-09 20:24:51 +0100

    better resize

--- packages/extension-search-view-worker/src/parts/Resize/Resize.ts
+++ packages/extension-search-view-worker/src/parts/Resize/Resize.ts
@@ -1,6 +1,37 @@
 import type { State } from '../State/State.ts'
+import * as GetFinalDeltaY from '../GetFinalDeltaY/GetFinalDeltaY.ts'
+import * as GetListHeight from '../GetListHeight/GetListHeight.ts'
+import * as GetNumberOfVisibleItems from '../GetNumberOfVisibleItems/GetNumberOfVisibleItems.ts'
+import * as GetScrollBarSize from '../GetScrollBarSize/GetScrollBarSize.ts'
+import * as ScrollBarFunctions from '../ScrollBarFunctions/ScrollBarFunctions.ts'
 
-export const resize = async (state: State, dimensions: any): Promise<State> => {
-  // TODO
-  return state
+interface Dimensions {
+  readonly x: number
+  readonly y: number
+  readonly width: number
+  readonly height: number
+}
+
+export const resize = async (state: State, dimensions: Dimensions): Promise<State> => {
+  const { items, itemHeight, headerHeight, minimumSliderSize } = state
+  const { x, y, width, height } = dimensions
+  const total = items.length
+  const listHeight = GetListHeight.getListHeight(total, itemHeight, height)
+  const contentHeight = total * itemHeight
+  const scrollBarHeight = GetScrollBarSize.getScrollBarSize(height, contentHeight, minimumSliderSize)
+  const numberOfVisible = GetNumberOfVisibleItems.getNumberOfVisibleItems(listHeight, itemHeight)
+  const maxLineY = Math.min(numberOfVisible, total)
+  const finalDeltaY = GetFinalDeltaY.getFinalDeltaY(listHeight, itemHeight, total)
+  const scrollBarY = ScrollBarFunctions.getScrollBarY(0, finalDeltaY, height - headerHeight, scrollBarHeight)
+  return {
+    ...state,
+    finalDeltaY,
+    maxLineY,
+    scrollBarHeight,
+    scrollBarY,
+    x,
+    y,
+    width,
+    height,
+  }
 }
