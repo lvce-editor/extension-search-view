c04b24ddcdbb3f29beb00b10ce6380f2ad6e09f7
Le Vivilet <levivilet@pm.me>
2025-11-24 22:50:38 +0100

    feature: simplify clear function (#493)

--- packages/extension-search-view-worker/src/parts/ClearSearchResults/ClearSearchResults.ts
+++ packages/extension-search-view-worker/src/parts/ClearSearchResults/ClearSearchResults.ts
@@ -1,15 +1,13 @@
 import { InputSource } from '@lvce-editor/constants'
 import type { State } from '../State/State.ts'
 import * as FocusId from '../FocusId/FocusId.ts'
-import * as ViewletExtensionsHandleInput from '../HandleInput/HandleInput.ts'
+import * as HandleChange from '../HandleChange/HandleChange.ts'
 
 export const clearSearchResults = (state: State): Promise<State> => {
-  return ViewletExtensionsHandleInput.handleInput(
-    {
-      ...state,
-      focus: FocusId.Input,
-    },
-    '',
-    InputSource.Script,
-  )
+  return HandleChange.handleChange(state, {
+    ...state,
+    focus: FocusId.Input,
+    searchValue: '',
+    inputSource: InputSource.Script,
+  })
 }
