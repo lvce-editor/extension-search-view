e8da29465c5ad76e8018eea265b983c523881b7a
Le Vivilet <levivilet@pm.me>
2025-11-26 12:28:02 +0100

    feature: add new context menu function (#499)

--- packages/extension-search-view-worker/src/parts/ContextMenu/ContextMenu.ts
+++ packages/extension-search-view-worker/src/parts/ContextMenu/ContextMenu.ts
@@ -1,9 +1,20 @@
+import { RendererWorker } from '@lvce-editor/rpc-registry'
+import type { ContextMenuProps } from '../ContextMenuProps/ContextMenuProps.ts'
 import * as Assert from '../Assert/Assert.ts'
-import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 export const show = async (x: number, y: number, id: number): Promise<void> => {
   Assert.number(x)
   Assert.number(y)
   Assert.number(id)
   await RendererWorker.showContextMenu(x, y, id)
+}
+
+export const show2 = async <T extends ContextMenuProps>(
+  uid: number,
+  menuId: ContextMenuProps['menuId'],
+  x: number,
+  y: number,
+  args: ContextMenuProps,
+): Promise<void> => {
+  await RendererWorker.showContextMenu2(uid, menuId, x, y, args)
 }
