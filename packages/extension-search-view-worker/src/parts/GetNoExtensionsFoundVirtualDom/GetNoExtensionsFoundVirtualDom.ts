8743badb9979df3d0369d1411e1c207a24f90104
Le Vivilet <levivilet@pm.me>
2025-12-06 13:46:24 +0100

    add class

--- packages/extension-search-view-worker/src/parts/GetNoExtensionsFoundVirtualDom/GetNoExtensionsFoundVirtualDom.ts
+++ packages/extension-search-view-worker/src/parts/GetNoExtensionsFoundVirtualDom/GetNoExtensionsFoundVirtualDom.ts
@@ -1,6 +1,7 @@
 import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
 
 export const getNoExtensionsFoundVirtualDom = (message: string): readonly VirtualDomNode[] => {
-  return [{ childCount: 1, type: VirtualDomElements.Div }, text(message)]
+  return [{ childCount: 1, className: ClassNames.NoExtensionsFoundMessage, type: VirtualDomElements.Div }, text(message)]
 }
