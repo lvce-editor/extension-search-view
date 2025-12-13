3de9c909481990e430bb60cf4e5a3090c2dbd7db
Le Vivilet <levivilet@pm.me>
2025-12-13 16:20:16 +0100

    feature: add outline class when focusedIndex is -1

--- packages/extension-search-view-worker/src/parts/GetExtensionsVirtualDom/GetExtensionsVirtualDom.ts
+++ packages/extension-search-view-worker/src/parts/GetExtensionsVirtualDom/GetExtensionsVirtualDom.ts
@@ -2,8 +2,8 @@ import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import type { VisibleItem } from '../VisibleItem/VisibleItem.ts'
 import * as GetExtensionsListVirtualDom from '../GetExtensionsListVirtualDom/GetExtensionsListVirtualDom.ts'
 
-export const getExtensionsVirtualDom = (visibleExtensions: readonly VisibleItem[]): readonly VirtualDomNode[] => {
-  const dom = GetExtensionsListVirtualDom.getExtensionsListVirtualDom(visibleExtensions)
+export const getExtensionsVirtualDom = (visibleExtensions: readonly VisibleItem[], focusOutline: boolean): readonly VirtualDomNode[] => {
+  const dom = GetExtensionsListVirtualDom.getExtensionsListVirtualDom(visibleExtensions, focusOutline)
   // TODO
   return dom
 }
