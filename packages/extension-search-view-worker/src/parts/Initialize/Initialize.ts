1315a0ffbdbe941770d449eda714aefcde039468
Le Vivilet <levivilet@pm.me>
2025-12-15 17:52:34 +0100

    feature: add extension management worker rpc

--- packages/extension-search-view-worker/src/parts/Initialize/Initialize.ts
+++ packages/extension-search-view-worker/src/parts/Initialize/Initialize.ts
@@ -1,5 +1,6 @@
 import { initializeExtensionHostWorker } from '../InitializeExtensionHostWorker/InitializeExtensionHostWorker.ts'
+import { initializeExtensionManagementWorker } from '../InitializeExtensionManagementWorker/InitializeExtensionManagementWorker.ts'
 
 export const initialize = async (): Promise<void> => {
-  await initializeExtensionHostWorker()
+  await Promise.all([initializeExtensionHostWorker(), initializeExtensionManagementWorker()])
 }
