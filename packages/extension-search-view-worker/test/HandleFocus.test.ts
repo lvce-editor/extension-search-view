66bb4ab79e6218adbad56dd2798a5e319c3914c8
Le Vivilet <levivilet@pm.me>
2026-01-10 20:30:50 +0100

    feature: add focus test

--- packages/extension-search-view-worker/test/HandleFocus.test.ts
+++ packages/extension-search-view-worker/test/HandleFocus.test.ts
@@ -1,13 +1,16 @@
 import { expect, test } from '@jest/globals'
-import { RendererWorker } from '@lvce-editor/rpc-registry'
+import type { State } from '../src/parts/State/State.ts'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import * as FocusId from '../src/parts/FocusId/FocusId.ts'
 import { handleFocus } from '../src/parts/HandleFocus/HandleFocus.ts'
 
-test.skip('handleFocus invokes RPC focus method', async () => {
-  const mockRpc = RendererWorker.registerMockRpc({
-    'Focus.setFocus'() {},
-  })
-  const state = createDefaultState()
-  expect(await handleFocus(state)).toBe(state)
-  expect(mockRpc.invocations).toEqual([['Focus.setFocus', 15]])
+test('handleFocus returns state with focus set to List', async () => {
+  const initialState: State = {
+    ...createDefaultState(),
+    focus: FocusId.Input,
+  }
+
+  const result = await handleFocus(initialState)
+  expect(result.focus).toBe(FocusId.List)
+  expect(result).not.toBe(initialState)
 })
