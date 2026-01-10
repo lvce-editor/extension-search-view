8d6c9df8d2844f431cd2268c8ddf6ada550ff932
Le Vivilet <levivilet@pm.me>
2026-01-10 20:37:33 +0100

    feature: improve filter position

--- packages/extension-search-view-worker/src/parts/HandleClickFilter/HandleClickFilter.ts
+++ packages/extension-search-view-worker/src/parts/HandleClickFilter/HandleClickFilter.ts
@@ -3,8 +3,10 @@ import type { State } from '../State/State.ts'
 import * as ContextMenu from '../ContextMenu/ContextMenu.ts'
 
 export const handleClickFilter = async (state: State): Promise<State> => {
-  const { uid, x, y } = state
-  await ContextMenu.show2(uid, MenuEntryId.ExtensionSearchFilter, x, y, {
+  const { headerHeight, uid, width, x, y } = state
+  const menuX = x + width + 80
+  const menuY = y + headerHeight + 100
+  await ContextMenu.show2(uid, MenuEntryId.ExtensionSearchFilter, menuX, menuY, {
     menuId: MenuEntryId.ExtensionSearchFilter,
   })
   return state
