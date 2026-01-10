af4f5e272e5170da59c9381db6d666abbe6da24c
Le Vivilet <levivilet@pm.me>
2026-01-10 17:38:48 +0100

    feature: add filter menu e2e test

---
+++ packages/e2e/src/extension-search.filter-menu.ts
@@ -0,0 +1,37 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const skip = 1
+
+export const test: Test = async ({ expect, ExtensionSearch, Locator }) => {
+  // arrange
+  await ExtensionSearch.open()
+  const extensionsView = Locator('.Extensions')
+  await expect(extensionsView).toBeVisible()
+
+  // act
+  const filterButton = extensionsView.locator('button[title="Filter"]')
+  // @ts-ignore
+  await filterButton.click()
+
+  // assert
+  const filterMenu = Locator('.Menu')
+  await expect(filterMenu).toBeVisible()
+  const menuItems = filterMenu.locator('.MenuItem')
+  await expect(menuItems).toHaveCount(8)
+  const menuItem1 = menuItems.nth(0)
+  await expect(menuItem1).toHaveText('@builtin')
+  const menuItem2 = menuItems.nth(1)
+  await expect(menuItem2).toHaveText('@disabled')
+  const menuItem3 = menuItems.nth(2)
+  await expect(menuItem3).toHaveText('@enabled')
+  const menuItem4 = menuItems.nth(3)
+  await expect(menuItem4).toHaveText('@installed')
+  const menuItem5 = menuItems.nth(4)
+  await expect(menuItem5).toHaveText('@outdated')
+  const menuItem6 = menuItems.nth(5)
+  await expect(menuItem6).toHaveText('@sort:installs')
+  const menuItem7 = menuItems.nth(6)
+  await expect(menuItem7).toHaveText('@id:')
+  const menuItem8 = menuItems.nth(7)
+  await expect(menuItem8).toHaveText('@category')
+}
