7a195fab80c884cd8fcd3c3a4d2428edff6cb19b
Le Vivilet <levivilet@pm.me>
2026-01-10 17:46:06 +0100

    feature: register handleClickFilter (#544)

---
+++ packages/extension-search-view-worker/src/parts/HandleDisableWorkspace/HandleDisableWorkspace.ts
@@ -0,0 +1,5 @@
+import type { State } from '../State/State.ts'
+
+export const handleDisableWorkspace = (state: State, id: string): State => {
+  return state
+}
---
+++ packages/extension-search-view-worker/src/parts/HandleEnableWorkspace/HandleEnableWorkspace.ts
@@ -0,0 +1,5 @@
+import type { State } from '../State/State.ts'
+
+export const handleEnableWorkspace = (state: State, id: string): State => {
+  return state
+}
