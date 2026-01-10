c9cc4afaebc252a697f8a37e78a4d69eae2ec053
Le Vivilet <levivilet@pm.me>
2026-01-10 17:46:56 +0100

    feature: implement basic toggleSuggest

--- packages/extension-search-view-worker/src/parts/ToggleSuggest/ToggleSuggest.ts
+++ packages/extension-search-view-worker/src/parts/ToggleSuggest/ToggleSuggest.ts
@@ -1,5 +1,8 @@
 import type { State } from '../State/State.ts'
 
 export const toggleSuggest = (state: State): State => {
-  return state
+  return {
+    ...state,
+    suggestOpen: !state.suggestOpen,
+  }
 }
--- packages/extension-search-view-worker/test/ToggleSuggest.test.ts
+++ packages/extension-search-view-worker/test/ToggleSuggest.test.ts
@@ -1,9 +1,12 @@
 import { expect, test } from '@jest/globals'
+import type { State } from '../src/parts/State/State.ts'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import { toggleSuggest } from '../src/parts/ToggleSuggest/ToggleSuggest.ts'
 
 test('toggleSuggest', () => {
-  const state = createDefaultState()
+  const state: State = { ...createDefaultState(), suggestOpen: false }
   const result = toggleSuggest(state)
-  expect(result).toEqual(state)
+  expect(result.suggestOpen).toBe(true)
+  const result2 = toggleSuggest(result)
+  expect(result2.suggestOpen).toBe(false)
 })
