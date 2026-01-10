88864af2b9bfbf718379fa2146f64df7f45bc064
Le Vivilet <levivilet@pm.me>
2026-01-10 17:41:07 +0100

    Discard changes to packages/extension-search-view-worker/src/parts/ContextMenuProps/ContextMenuProps.ts

--- packages/extension-search-view-worker/src/parts/ContextMenuProps/ContextMenuProps.ts
+++ packages/extension-search-view-worker/src/parts/ContextMenuProps/ContextMenuProps.ts
@@ -1,6 +1,4 @@
 import type { MenuEntryId } from '@lvce-editor/constants'
-// eslint-disable-next-line @typescript-eslint/consistent-type-imports
-import * as MenuEntryIdLocal from '../MenuEntryId/MenuEntryId.ts'
 
 export interface ContextMenuPropsBase {
   readonly menuId: number
@@ -10,8 +8,4 @@ export interface ContextMenuPropsExplorer extends ContextMenuPropsBase {
   readonly menuId: typeof MenuEntryId.ManageExtension
 }
 
-export interface ContextMenuPropsExtensionSearchFilter extends ContextMenuPropsBase {
-  readonly menuId: typeof MenuEntryIdLocal.ExtensionSearchFilter
-}
-
-export type ContextMenuProps = ContextMenuPropsExplorer | ContextMenuPropsExtensionSearchFilter
+export type ContextMenuProps = ContextMenuPropsExplorer
