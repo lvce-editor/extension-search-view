6a352d8933114044de62464c9cc22f15c5ee434c
Le Vivilet <levivilet@pm.me>
2026-01-10 16:57:44 +0100

    feature: use native toSorted function

--- packages/extension-search-view-worker/src/parts/SortExtensions/SortExtensions.ts
+++ packages/extension-search-view-worker/src/parts/SortExtensions/SortExtensions.ts
@@ -1,7 +1,6 @@
 import type { ExtensionListItem } from '../ExtensionListItem/ExtensionListItem.ts'
-import * as Arrays from '../Arrays/Arrays.ts'
 import * as CompareExtension from '../CompareExtension/CompareExtension.ts'
 
 export const sortExtensions = (extensions: readonly ExtensionListItem[]): readonly ExtensionListItem[] => {
-  return Arrays.toSorted(extensions, CompareExtension.compareExtension)
+  return extensions.toSorted(CompareExtension.compareExtension)
 }
