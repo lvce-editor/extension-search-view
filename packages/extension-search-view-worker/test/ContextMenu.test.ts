6200b8c4750db9045ac604fc016e43c53dade375
Le Vivilet <levivilet@pm.me>
2026-01-10 16:53:33 +0100

    feature: add menu test

--- packages/extension-search-view-worker/test/ContextMenu.test.ts
+++ packages/extension-search-view-worker/test/ContextMenu.test.ts
@@ -1,6 +1,7 @@
 import { expect, test } from '@jest/globals'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 import * as ContextMenu from '../src/parts/ContextMenu/ContextMenu.ts'
+import * as MenuEntryId from '../src/parts/MenuEntryId/MenuEntryId.ts'
 
 test('show function validates input parameters', async () => {
   const mockRpc = RendererWorker.registerMockRpc({
@@ -12,4 +13,14 @@ test('show function validates input parameters', async () => {
     ['ContextMenu.show', 100, 200, 1],
     ['ContextMenu.show', 0, 0, 0],
   ])
+})
+
+test('show2 function calls RendererWorker.showContextMenu2 with correct parameters', async () => {
+  const mockRpc = RendererWorker.registerMockRpc({
+    'ContextMenu.show2'() {},
+  })
+  await ContextMenu.show2(123, MenuEntryId.ManageExtension, 100, 200, {
+    menuId: MenuEntryId.ManageExtension,
+  })
+  expect(mockRpc.invocations).toEqual([['ContextMenu.show2', 123, MenuEntryId.ManageExtension, 100, 200, { menuId: MenuEntryId.ManageExtension }]])
 })
