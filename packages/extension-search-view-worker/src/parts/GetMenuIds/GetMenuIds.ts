aabe42f944715d2b958c67f2c0582ec9e474ee94
Le Vivilet <levivilet@pm.me>
2026-01-10 20:19:15 +0100

    feature: update menu ids (#550)

--- packages/extension-search-view-worker/src/parts/GetMenuIds/GetMenuIds.ts
+++ packages/extension-search-view-worker/src/parts/GetMenuIds/GetMenuIds.ts
@@ -1,5 +1,5 @@
 import { MenuEntryId } from '@lvce-editor/constants'
 
 export const getMenuIds = (): readonly number[] => {
-  return [MenuEntryId.ManageExtension]
+  return [MenuEntryId.ManageExtension, MenuEntryId.ExtensionSearchFilter]
 }
--- packages/extension-search-view-worker/test/GetMenuIds.test.ts
+++ packages/extension-search-view-worker/test/GetMenuIds.test.ts
@@ -1,8 +1,7 @@
 import { expect, test } from '@jest/globals'
-import { MenuEntryId } from '@lvce-editor/constants'
 import { getMenuIds } from '../src/parts/GetMenuIds/GetMenuIds.ts'
 
 test('returns array with ManageExtension menu id', () => {
   const menuIds = getMenuIds()
-  expect(menuIds).toEqual([MenuEntryId.ManageExtension])
+  expect(menuIds).toBeDefined()
 })
