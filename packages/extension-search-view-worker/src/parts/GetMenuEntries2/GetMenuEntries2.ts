049a53fb9c3be7da406389489beba5c647a9fbc9
Le Vivilet <levivilet@pm.me>
2026-01-10 20:19:44 +0100

    feature: update getMenuEntries2

--- packages/extension-search-view-worker/src/parts/GetMenuEntries2/GetMenuEntries2.ts
+++ packages/extension-search-view-worker/src/parts/GetMenuEntries2/GetMenuEntries2.ts
@@ -1,7 +1,16 @@
+import { MenuEntryId } from '@lvce-editor/constants'
+import type { ContextMenuProps } from '../ContextMenuProps/ContextMenuProps.ts'
 import type { MenuEntry } from '../MenuEntry/MenuEntry.ts'
 import type { State } from '../State/State.ts'
 import { getMenuEntries } from '../GetMenuEntries/GetMenuEntries.ts'
+import { getMenuEntriesFilter } from '../GetMenuEntriesFilter/GetMenuEntriesFilter.ts'
 
-export const getMenuEntries2 = (state: State): readonly MenuEntry[] => {
-  return getMenuEntries()
+export const getMenuEntries2 = (state: State, props: ContextMenuProps): readonly MenuEntry[] => {
+  const { menuId } = props
+  switch (menuId) {
+    case MenuEntryId.ExtensionSearchFilter:
+      return getMenuEntriesFilter()
+    default:
+      return getMenuEntries()
+  }
 }
