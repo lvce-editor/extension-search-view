1080c90a30bbf60b3bb2e5358b4ee97c1f113257
Le Vivilet <levivilet@pm.me>
2026-01-10 16:58:29 +0100

    feature: split up compar function (#535)

--- packages/extension-search-view-worker/src/parts/CompareExtension/CompareExtension.ts
+++ packages/extension-search-view-worker/src/parts/CompareExtension/CompareExtension.ts
@@ -1,5 +1,13 @@
 import type { ExtensionListItem } from '../ExtensionListItem/ExtensionListItem.ts'
 
+export const compareName = (extensionA: ExtensionListItem, extensionB: ExtensionListItem): number => {
+  return extensionA.name.localeCompare(extensionB.name)
+}
+
+export const compareId = (extensionA: ExtensionListItem, extensionB: ExtensionListItem): number => {
+  return extensionA.id.localeCompare(extensionB.id)
+}
+
 export const compareExtension = (extensionA: ExtensionListItem, extensionB: ExtensionListItem): number => {
   return extensionA.name.localeCompare(extensionB.name) || extensionA.id.localeCompare(extensionB.id)
 }
