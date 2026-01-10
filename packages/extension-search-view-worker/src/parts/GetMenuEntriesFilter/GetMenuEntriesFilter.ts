665d5ee517630110f9bc00ccb5582e4a76774131
Le Vivilet <levivilet@pm.me>
2026-01-10 20:46:37 +0100

    feature: update filter menu

--- packages/extension-search-view-worker/src/parts/GetMenuEntriesFilter/GetMenuEntriesFilter.ts
+++ packages/extension-search-view-worker/src/parts/GetMenuEntriesFilter/GetMenuEntriesFilter.ts
@@ -5,16 +5,46 @@ import * as MenuItemFlags from '../MenuItemFlags/MenuItemFlags.ts'
 export const getMenuEntriesFilter = (): readonly MenuEntry[] => {
   return [
     {
-      command: 'SearchExtensions.filterByCategory',
+      command: 'SearchExtensions.filterByFeatured',
       flags: MenuItemFlags.None,
-      id: 'filterByCategory',
-      label: ExtensionStrings.category(),
+      id: 'filterByFeatured',
+      label: ExtensionStrings.featured(),
     },
     {
-      command: 'SearchExtensions.filterById',
+      command: 'SearchExtensions.filterByMcpServers',
       flags: MenuItemFlags.None,
-      id: 'filterById',
-      label: ExtensionStrings.id(),
+      id: 'filterByMcpServers',
+      label: ExtensionStrings.mcpServers(),
+    },
+    {
+      command: 'SearchExtensions.filterByMostPopular',
+      flags: MenuItemFlags.None,
+      id: 'filterByMostPopular',
+      label: ExtensionStrings.mostPopular(),
+    },
+    {
+      command: 'SearchExtensions.filterByRecentlyPublished',
+      flags: MenuItemFlags.None,
+      id: 'filterByRecentlyPublished',
+      label: ExtensionStrings.recentlyPublished(),
+    },
+    {
+      command: 'SearchExtensions.filterByRecommended',
+      flags: MenuItemFlags.None,
+      id: 'filterByRecommended',
+      label: ExtensionStrings.recommended(),
+    },
+    {
+      command: '',
+      flags: MenuItemFlags.Separator,
+      id: 'separator1',
+      label: '',
+    },
+    {
+      command: 'SearchExtensions.filterByCategory',
+      flags: MenuItemFlags.SubMenu,
+      id: 'filterByCategory',
+      label: ExtensionStrings.category(),
     },
     {
       command: 'SearchExtensions.filterByInstalled',
@@ -23,10 +53,46 @@ export const getMenuEntriesFilter = (): readonly MenuEntry[] => {
       label: ExtensionStrings.installed(),
     },
     {
+      command: 'SearchExtensions.filterByUpdates',
+      flags: MenuItemFlags.None,
+      id: 'filterByUpdates',
+      label: ExtensionStrings.updates(),
+    },
+    {
+      command: 'SearchExtensions.filterByBuiltin',
+      flags: MenuItemFlags.None,
+      id: 'filterByBuiltin',
+      label: ExtensionStrings.builtIn(),
+    },
+    {
+      command: 'SearchExtensions.filterByEnabled',
+      flags: MenuItemFlags.None,
+      id: 'filterByEnabled',
+      label: ExtensionStrings.enabled(),
+    },
+    {
       command: 'SearchExtensions.filterByDisabled',
       flags: MenuItemFlags.None,
       id: 'filterByDisabled',
       label: ExtensionStrings.disabled(),
+    },
+    {
+      command: 'SearchExtensions.filterByWorkspaceUnsupported',
+      flags: MenuItemFlags.None,
+      id: 'filterByWorkspaceUnsupported',
+      label: ExtensionStrings.workspaceUnsupported(),
+    },
+    {
+      command: '',
+      flags: MenuItemFlags.Separator,
+      id: 'separator2',
+      label: '',
+    },
+    {
+      command: 'SearchExtensions.filterBySortBy',
+      flags: MenuItemFlags.SubMenu,
+      id: 'filterBySortBy',
+      label: ExtensionStrings.sortBy(),
     },
   ]
 }
