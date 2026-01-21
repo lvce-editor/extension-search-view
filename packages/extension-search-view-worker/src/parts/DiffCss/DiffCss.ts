35554457ae5087dfa7e9244d2eec106027499fef
Le Vivilet <levivilet@pm.me>
2026-01-21 23:50:20 +0100

    feature: update css diff

--- packages/extension-search-view-worker/src/parts/DiffCss/DiffCss.ts
+++ packages/extension-search-view-worker/src/parts/DiffCss/DiffCss.ts
@@ -1,5 +1,10 @@
 import type { State } from '../State/State.ts'
 
 export const isEqual = (oldState: State, newState: State): boolean => {
-  return newState.scrollBarHeight === newState.scrollBarHeight && oldState.scrollBarY === newState.scrollBarY
+  return (
+    newState.scrollBarHeight === newState.scrollBarHeight &&
+    oldState.scrollBarY === newState.scrollBarY &&
+    oldState.initial === newState.initial &&
+    oldState.deltaY === newState.deltaY
+  )
 }
