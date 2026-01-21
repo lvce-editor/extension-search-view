34e74d7f77a2ca15967a06c99ec57c02c5906bd7
Le Vivilet <levivilet@pm.me>
2026-01-21 23:01:18 +0100

    feature: if initial is true, return empty dom (#577)

--- packages/extension-search-view-worker/src/parts/RenderItems2/RenderItems2.ts
+++ packages/extension-search-view-worker/src/parts/RenderItems2/RenderItems2.ts
@@ -1,7 +1,12 @@
+import { ViewletCommand } from '@lvce-editor/constants'
 import type { State } from '../State/State.ts'
 import { getExtensionsViewVirtualDom } from '../GetExtensionsViewVirtualDom/GetExtensionsViewVirtualDom.ts'
 
 export const renderItems2 = (newState: State): readonly any[] => {
+  const { initial, uid } = newState
+  if (initial) {
+    return [ViewletCommand.SetDom2, []]
+  }
   const dom = getExtensionsViewVirtualDom(newState)
-  return ['Viewlet.setDom2', newState.uid, dom]
+  return [ViewletCommand.SetDom2, uid, dom]
 }
