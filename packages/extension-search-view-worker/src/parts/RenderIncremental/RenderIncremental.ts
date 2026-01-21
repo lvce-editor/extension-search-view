a0097eec39d5273d3a461d9c47280715f978fc7e
Le Vivilet <levivilet@pm.me>
2026-01-21 22:31:52 +0100

    fix

--- packages/extension-search-view-worker/src/parts/RenderIncremental/RenderIncremental.ts
+++ packages/extension-search-view-worker/src/parts/RenderIncremental/RenderIncremental.ts
@@ -1,11 +1,12 @@
+import { ViewletCommand } from '@lvce-editor/constants'
 import { diffTree } from '@lvce-editor/virtual-dom-worker'
 import type { State } from '../State/State.ts'
-import { renderItems } from '../RenderItems/RenderItems.ts'
+import { renderItems2 } from '../RenderItems2/RenderItems2.ts'
 
 // TODO cache rendered dom so that it can be used for dom diffing
 export const renderIncremental = (oldState: State, newState: State): readonly any[] => {
-  const oldDom = renderItems(oldState)[1]
-  const newDom = renderItems(newState)[1]
+  const oldDom = renderItems2(oldState)[2]
+  const newDom = renderItems2(newState)[2]
   const patches = diffTree(oldDom, newDom)
-  return ['Viewlet.setPatches', newState.uid, patches]
+  return [ViewletCommand.SetPatches, newState.uid, patches]
 }
