437a5aeb5ffeac54900f3e1a37a80dd5fe939d31
Le Vivilet <levivilet@pm.me>
2026-01-23 16:13:11 +0100

    feature: add settings button e2e test (#608)

---
+++ packages/e2e/src/extension-search.settings-button.ts
@@ -0,0 +1,35 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const skip = 1
+
+export const test: Test = async ({ expect, ExtensionSearch, Locator }) => {
+  // arrange
+  await ExtensionSearch.open()
+  const extensionsView = Locator('.Extensions')
+  await expect(extensionsView).toBeVisible()
+  await ExtensionSearch.handleInput('atom')
+
+  // act
+  const settingsButton = Locator('.ExtensionSettingsButton').first()
+  await expect(settingsButton).toBeVisible()
+  await settingsButton.click()
+
+  // assert
+  const contextMenu = Locator('.Menu')
+  await expect(contextMenu).toBeVisible()
+  const menuItems = contextMenu.locator('.MenuItem')
+  const menuItem1 = menuItems.nth(0)
+  await expect(menuItem1).toHaveText('Enable')
+  const menuItem2 = menuItems.nth(1)
+  await expect(menuItem2).toHaveText('Enable Workspace')
+  const menuItem3 = menuItems.nth(3)
+  await expect(menuItem3).toHaveText('Disable')
+  const menuItem4 = menuItems.nth(4)
+  await expect(menuItem4).toHaveText('Disable Workspace')
+  const menuItem5 = menuItems.nth(6)
+  await expect(menuItem5).toHaveText('Install Another Version')
+  const menuItem6 = menuItems.nth(7)
+  await expect(menuItem6).toHaveText('Copy')
+  const menuItem7 = menuItems.nth(8)
+  await expect(menuItem7).toHaveText('Copy Extension Id')
+}
