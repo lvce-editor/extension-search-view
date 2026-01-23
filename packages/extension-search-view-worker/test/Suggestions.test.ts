a17b83803909540c577feed9ddb93e5a84d9ba30
Le Vivilet <levivilet@pm.me>
2026-01-23 16:07:31 +0100

    feature: remove unused test (#602)

--- packages/extension-search-view-worker/test/Suggestions.test.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@jest/globals'
-import { Suggestions } from '../src/parts/Suggestions/Suggestions.ts'
-
-test('Suggestions - should have correct suggestions', () => {
-  expect(Suggestions).toEqual(['@builtin', '@disabled', '@enabled', '@installed', '@outdated', '@sort:installs', '@id:', '@category'])
-})
