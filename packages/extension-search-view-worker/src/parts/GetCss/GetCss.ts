16d7dc7c119b563d19addf20b3710d6024879371
Le Vivilet <levivilet@pm.me>
2026-01-23 12:33:01 +0100

    feature: round scrollbar height (#589)

--- packages/extension-search-view-worker/src/parts/GetCss/GetCss.ts
+++ packages/extension-search-view-worker/src/parts/GetCss/GetCss.ts
@@ -3,9 +3,10 @@ import type { State } from '../State/State.ts'
 export const getCss = (state: State): string => {
   const { deltaY, itemHeight, scrollBarHeight, scrollBarY } = state
   const relative = deltaY % itemHeight
+  const roundedScrollBarY = Math.round(scrollBarY)
   return `.Extensions .ScrollBarThumb {
   height: ${scrollBarHeight}px;
-  top: ${scrollBarY}px;
+  top: ${roundedScrollBarY}px;
 }
 
 .Extensions .ListItems {
