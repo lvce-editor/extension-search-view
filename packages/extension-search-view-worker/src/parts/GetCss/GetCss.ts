f807ce1f874ee0d5486e58d74e6a6dc94007a012
Le Vivilet <levivilet@pm.me>
2026-01-23 12:39:35 +0100

    feature: use translate property instead of top

--- packages/extension-search-view-worker/src/parts/GetCss/GetCss.ts
+++ packages/extension-search-view-worker/src/parts/GetCss/GetCss.ts
@@ -2,15 +2,15 @@ import type { State } from '../State/State.ts'
 
 export const getCss = (state: State): string => {
   const { deltaY, itemHeight, scrollBarHeight, scrollBarY } = state
-  const relative = deltaY % itemHeight
+  const relative = -(deltaY % itemHeight)
   const roundedScrollBarY = Math.round(scrollBarY)
   return `.Extensions .ScrollBarThumb {
   height: ${scrollBarHeight}px;
-  top: ${roundedScrollBarY}px;
+  translate: 0 ${roundedScrollBarY}px;
 }
 
 .Extensions .ListItems {
-  top: -${relative}px;
+  translate: 0 ${relative}px;
 
 .ExtensionListItem {
   position: relative !important;
