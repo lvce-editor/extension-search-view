4ac7ba23e5feb541ba9b9c9ee2f1a4efa3965a36
Le Vivilet <levivilet@pm.me>
2026-01-23 18:12:44 +0100

    feature: rename function (#611)

--- packages/extension-search-view-worker/test/NormalizeExtensions.test.ts
+++ packages/extension-search-view-worker/test/NormalizeExtensions.test.ts
@@ -1,11 +1,11 @@
 import { test, expect } from '@jest/globals'
-import { normalizeExtension } from '../src/parts/NormalizeExtensions/NormalizeExtensions.ts'
+import { normalizeExtensions } from '../src/parts/NormalizeExtensions/NormalizeExtensions.ts'
 
 test('normalizeExtension should normalize multiple extensions', () => {
   const extension1 = { description: 'Desc 1', id: '1', name: 'Extension 1', publisher: 'Publisher 1', size: 1000, updatedDate: 1000 }
   const extension2 = { description: 'Desc 2', id: '2', name: 'Extension 2', publisher: 'Publisher 2', size: 2000, updatedDate: 2000 }
 
-  const result = normalizeExtension([extension1, extension2], 1, '/assets')
+  const result = normalizeExtensions([extension1, extension2], 1, '/assets')
 
   expect(result).toHaveLength(2)
   expect(result[0]).toEqual({
@@ -33,6 +33,6 @@ test('normalizeExtension should normalize multiple extensions', () => {
 })
 
 test('normalizeExtension should handle empty array', () => {
-  const result = normalizeExtension([], 1, '/assets')
+  const result = normalizeExtensions([], 1, '/assets')
   expect(result).toEqual([])
 })
