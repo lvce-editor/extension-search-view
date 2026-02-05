044c4d9bc134687cc08732ad48bf467cd4668b6a
Le Vivilet <levivilet@pm.me>
2026-02-05 13:10:16 +0100

    fix

--- packages/extension-search-view-worker/test/ContextMenu.test.ts
+++ packages/extension-search-view-worker/test/ContextMenu.test.ts
@@ -3,18 +3,6 @@ import { MenuEntryId } from '@lvce-editor/constants'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 import * as ContextMenu from '../src/parts/ContextMenu/ContextMenu.ts'
 
-test('show function validates input parameters', async () => {
-  using mockRpc = RendererWorker.registerMockRpc({
-    'ContextMenu.show'() {},
-  })
-  await expect(ContextMenu.show(100, 200, 1)).resolves.not.toThrow()
-  await expect(ContextMenu.show(0, 0, 0)).resolves.not.toThrow()
-  expect(mockRpc.invocations).toEqual([
-    ['ContextMenu.show', 100, 200, 1],
-    ['ContextMenu.show', 0, 0, 0],
-  ])
-})
-
 test('show2 function calls RendererWorker.showContextMenu2 with correct parameters', async () => {
   using mockRpc = RendererWorker.registerMockRpc({
     'ContextMenu.show2'() {},
