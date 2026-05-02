0128958e0fda7f04da8852378d8e13c3b4de2e12
Le Vivilet <levivilet@pm.me>
2026-05-02 11:24:00 +0200

    fix

--- packages/extension-search-view-worker/src/parts/ExtensionHostWorker/ExtensionHostWorker.ts
+++ packages/extension-search-view-worker/src/parts/ExtensionHostWorker/ExtensionHostWorker.ts
@@ -1,4 +1,4 @@
 import { ExtensionHost } from '@lvce-editor/rpc-registry'
 
-export const { getRuntimeStatus } = ExtensionHost
+export const getRuntimeStatus: typeof ExtensionHost.getRuntimeStatus = ExtensionHost.getRuntimeStatus
 export const { invoke, set } = ExtensionHost
