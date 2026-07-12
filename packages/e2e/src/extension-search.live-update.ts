0e0c86619c2cd5584e54112506e457ee5e706d47
Le Vivilet <levivilet@pm.me>
2026-07-12 17:45:51 +0000

    fix: assert live extension list container

--- packages/e2e/src/extension-search.live-update.ts
+++ packages/e2e/src/extension-search.live-update.ts
@@ -6,15 +6,15 @@ export const test: Test = async ({ expect, Extension, ExtensionSearch, Locator }
   // arrange
   await ExtensionSearch.open()
   await ExtensionSearch.handleInput('@id:test.dynamic-extension')
-  const extensions = Locator('.Extensions .ExtensionListItem')
-  await expect(extensions).toHaveCount(0)
+  const listItems = Locator('.Extensions .ListItems')
+  await expect(listItems).toHaveCount(0)
 
   // act
   const extensionUri = new URL('../fixtures/dynamic-extension', import.meta.url).href
   await Extension.addWebExtension(extensionUri)
 
   // assert
-  await expect(extensions).toHaveCount(1)
-  const extensionName = extensions.locator('.ExtensionListItemName')
+  await expect(listItems).toHaveCount(1)
+  const extensionName = listItems.locator('.ExtensionListItemName')
   await expect(extensionName).toHaveText('Dynamic Extension')
 }
