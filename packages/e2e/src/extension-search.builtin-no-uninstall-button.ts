c5ff693fa5bde0c5d7ebed646a5505422b232093
Le Vivilet <levivilet@pm.me>
2026-07-13 13:44:29 +0000

    test: wait for extension search content

--- packages/e2e/src/extension-search.builtin-no-uninstall-button.ts
+++ packages/e2e/src/extension-search.builtin-no-uninstall-button.ts
@@ -5,12 +5,11 @@ export const skip = 0
 export const test: Test = async ({ Command, expect, ExtensionSearch, Locator }) => {
   const extensionId = 'builtin.theme-atom-one-dark'
   await ExtensionSearch.open()
-  const extensionsView = Locator('.Extensions')
-  await expect(extensionsView).toBeVisible()
+  const extensionItems = Locator('.ExtensionListItem')
+  await expect(extensionItems).toHaveCount(10)
   await ExtensionSearch.clearSearchResults()
   const query = `@id:${extensionId}`
   await Command.execute('Extensions.handleInput', query, 1, query.length)
-  const extensionItems = Locator('.ExtensionListItem')
   await expect(extensionItems).toHaveCount(1)
 
   await Command.execute('Extensions.setExtensionStatus', extensionId, 'enabled')
