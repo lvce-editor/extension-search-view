40143060f6167c30c9bce7fbc65c88c473e78c8a
Le Vivilet <levivilet@pm.me>
2026-07-25 23:24:51 +0200

    feature: use dialog worker directly (#691)

--- packages/extension-search-view-worker/test/Disable.test.ts
+++ packages/extension-search-view-worker/test/Disable.test.ts
@@ -1,10 +1,10 @@
 import { expect, test } from '@jest/globals'
-import { RendererWorker } from '@lvce-editor/rpc-registry'
+import { DialogWorker } from '@lvce-editor/rpc-registry'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import { disable } from '../src/parts/Disable/Disable.ts'
 
 test('disable shows placeholder confirm and returns state', async () => {
-  using mockRpc = RendererWorker.registerMockRpc({
+  using mockRpc = DialogWorker.registerMockRpc({
     'ConfirmPrompt.prompt'() {
       return false
     },
