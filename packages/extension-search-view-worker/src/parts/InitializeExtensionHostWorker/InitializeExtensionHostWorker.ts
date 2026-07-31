668de009bd026738f2f1796564cd53810e28d3d3
Le Vivilet <levivilet@pm.me>
2026-07-31 14:27:33 -0700

    fix: remove unused extension host connection (#701)

--- packages/extension-search-view-worker/src/parts/InitializeExtensionHostWorker/InitializeExtensionHostWorker.ts
+++
@@ -1,7 +0,0 @@
-import { createExtensionHostWorkerRpc } from '../CreateExtensionHostWorkerRpc/CreateExtensionHostWorkerRpc.ts'
-import * as ExtensionHostWorker from '../ExtensionHostWorker/ExtensionHostWorker.ts'
-
-export const initializeExtensionHostWorker = async (): Promise<void> => {
-  const rpc = await createExtensionHostWorkerRpc()
-  ExtensionHostWorker.set(rpc)
-}
