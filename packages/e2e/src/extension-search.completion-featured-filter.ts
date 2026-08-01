93bc9933685b3d8caddfb9d646b490689ad29ddb
Le Vivilet <levivilet@pm.me>
2026-08-01 09:36:42 +0000

    feature: use page objects in new extension search tests

--- packages/e2e/src/extension-search.completion-featured-filter.ts
+++ packages/e2e/src/extension-search.completion-featured-filter.ts
@@ -2,10 +2,10 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const skip = 0
 
-export const test: Test = async ({ Command, expect, ExtensionSearch, Locator }) => {
+export const test: Test = async ({ expect, ExtensionSearch, Locator }) => {
   await ExtensionSearch.open()
   await ExtensionSearch.clearSearchResults()
-  await Command.execute('Extensions.handleInput', '@feat', 1, 5)
+  await ExtensionSearch.handleInput('@feat', 1, 5)
   const items = Locator('.ExtensionSearchCompletionItem')
   await expect(items).toHaveCount(1)
   await expect(items).toHaveText('@featured')
--- packages/e2e/src/extension-search.focus-moves-active-id.ts
+++ packages/e2e/src/extension-search.focus-moves-active-id.ts
@@ -2,12 +2,12 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const skip = 0
 
-export const test: Test = async ({ Command, expect, ExtensionSearch, Locator }) => {
+export const test: Test = async ({ expect, ExtensionSearch, Locator }) => {
   await ExtensionSearch.open()
   await ExtensionSearch.handleInput('@category:"themes"')
-  await Command.execute('Extensions.focusFirst')
+  await ExtensionSearch.focusFirst()
   const activeItem = Locator('.ExtensionListItem#ExtensionActive')
   await expect(activeItem.locator('.ExtensionListItemName')).toHaveText('Ayu Theme')
-  await Command.execute('Extensions.focusNext')
+  await ExtensionSearch.focusNext()
   await expect(activeItem.locator('.ExtensionListItemName')).toHaveText('Cobalt 2 Theme')
 }
