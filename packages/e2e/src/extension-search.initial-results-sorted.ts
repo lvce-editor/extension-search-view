e1a1ad15e22f556de30079952295134a9c0217db
Le Vivilet <levivilet@pm.me>
2026-08-01 01:26:26 -0700

    Add 20 extension search e2e tests (#710)

---
+++ packages/e2e/src/extension-search.initial-results-sorted.ts
@@ -0,0 +1,12 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const skip = 0
+
+export const test: Test = async ({ expect, ExtensionSearch, Locator }) => {
+  await ExtensionSearch.open()
+  await ExtensionSearch.clearSearchResults()
+  const names = Locator('.ExtensionListItemName')
+  const secondName = names.nth(1)
+  await expect(names.first()).toHaveText('Atom One Dark Theme')
+  await expect(secondName).toHaveText('Ayu Theme')
+}
