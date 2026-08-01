49f87b8c56701cb1b810c2bf6236dc452af5aa57
Le Vivilet <levivilet@pm.me>
2026-08-01 10:15:07 +0200

    feature: simplify list class

--- packages/extension-search-view-worker/src/parts/GetExtensionsListVirtualDom/GetExtensionsListVirtualDom.ts
+++ packages/extension-search-view-worker/src/parts/GetExtensionsListVirtualDom/GetExtensionsListVirtualDom.ts
@@ -1,20 +1,24 @@
 import { mergeClassNames, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
-import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
-import type { VisibleItem } from '../VisibleItem/VisibleItem.ts'
 import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as ExtensionStrings from '../ExtensionStrings/ExtensionStrings.ts'
 import * as GetExtensionsListItemVirtualDom from '../GetExtensionsListItemVirtualDom/GetExtensionsListItemVirtualDom.ts'
 import * as TabIndex from '../TabIndex/TabIndex.ts'
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import type { VisibleItem } from '../VisibleItem/VisibleItem.ts'
+
+const getListClassName = (focusOutline: boolean): string => {
+  const className = focusOutline ? mergeClassNames(ClassNames.ListItems, ClassNames.FocusOutline) : ClassNames.ListItems
+  return className
+}
 
 export const getExtensionsListVirtualDom = (visibleExtensions: readonly VisibleItem[], focusOutline: boolean): readonly VirtualDomNode[] => {
-  const className = focusOutline ? mergeClassNames(ClassNames.ListItems, ClassNames.FocusOutline) : ClassNames.ListItems
   const dom: readonly VirtualDomNode[] = [
     {
       ariaLabel: ExtensionStrings.extensions(),
       childCount: visibleExtensions.length,
-      className,
+      className: getListClassName(focusOutline),
       onBlur: DomEventListenerFunctions.HandleBlur,
       onContextmenu: DomEventListenerFunctions.HandleContextMenu,
       onContextMenu: DomEventListenerFunctions.HandleContextMenu,
