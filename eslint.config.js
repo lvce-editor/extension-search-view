491938257219ff0a18d9834159f1fdc3e9f8ab0b
Le Vivilet <levivilet@pm.me>
2026-08-01 01:14:31 -0700

    feature: add e2e rules (#705)

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import * as config from '@lvce-editor/eslint-config'
 import * as actions from '@lvce-editor/eslint-plugin-github-actions/dist/index.js'
 
 export default [
+  ...config.recommendedE2e,
   ...config.default,
   ...config.recommendedVirtualDom,
   ...actions.default,
