e9c64c19da9f9916809d729e997e17def7bc941a
Le Vivilet <levivilet@pm.me>
2026-08-24 11:24:59 +0000

    fix: satisfy builtin metadata test lint

--- packages/extension-search-view-worker/test/GetBuiltin.test.ts
+++ packages/extension-search-view-worker/test/GetBuiltin.test.ts
@@ -3,14 +3,14 @@ import { getBuiltin } from '../src/parts/GetBuiltin/GetBuiltin.ts'
 
 test.each([{ builtin: true, id: 'publisher.extension' }, { id: 'publisher.extension', isBuiltin: true }, { id: 'builtin.gpt-voice' }])(
   'returns true for builtin extension %#',
-  (extension) => {
+  (extension: unknown) => {
     expect(getBuiltin(extension)).toBe(true)
   },
 )
 
 test.each([null, undefined, 'builtin.extension', {}, { builtin: false }, { id: 1 }, { id: 'publisher.extension' }, { isBuiltin: false }])(
   'returns false for non-builtin extension %#',
-  (extension) => {
+  (extension: unknown) => {
     expect(getBuiltin(extension)).toBe(false)
   },
 )
