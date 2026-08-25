b66ffd691b09993926199e46d886614ea6dc1736
Le Vivilet <levivilet@pm.me>
2026-08-25 21:02:36 +0000

    test: keep light theme active

--- packages/e2e/src/extension-search.disabled-text-color.ts
+++ packages/e2e/src/extension-search.disabled-text-color.ts
@@ -4,7 +4,6 @@ export const test: Test = async ({ Command, expect, ExtensionSearch, Locator }) 
   await Command.execute('ColorTheme.setColorTheme', 'ayu')
   await ExtensionSearch.open()
   await ExtensionSearch.setExtensionStatus('builtin.theme-atom-one-dark', 'disabled')
-  await ExtensionSearch.setExtensionStatus('builtin.theme-ayu', 'disabled')
   await ExtensionSearch.handleInput('@disabled')
 
   const listItem = Locator('.ExtensionListItemDisabled:not(.ExtensionActive)').first()
