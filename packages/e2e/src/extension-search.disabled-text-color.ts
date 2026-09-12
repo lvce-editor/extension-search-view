1a03199cb4eba9314623ca32c35a0bef328f56de
levivilet <72156503+levivilet@users.noreply.github.com>
2026-09-12 10:49:26 +0000

    feature: use color theme page object in extension search test

--- packages/e2e/src/extension-search.disabled-text-color.ts
+++ packages/e2e/src/extension-search.disabled-text-color.ts
@@ -2,12 +2,12 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const skip = navigator.platform === 'Win32'
 
-export const test: Test = async ({ Command, expect, ExtensionSearch, Locator }) => {
+export const test: Test = async ({ ColorTheme, expect, ExtensionSearch, Locator }) => {
   await ExtensionSearch.open()
   await ExtensionSearch.setExtensionStatus('builtin.theme-atom-one-dark', 'disabled')
   await ExtensionSearch.setExtensionStatus('builtin.theme-ayu', 'disabled')
   await ExtensionSearch.handleInput('@disabled')
-  await Command.execute('ColorTheme.setColorTheme', 'ayu')
+  await ColorTheme.setColorTheme('ayu')
 
   const listItem = Locator('.ExtensionListItemDisabled:not(.ExtensionActive)').first()
   await expect(listItem).toBeVisible()
