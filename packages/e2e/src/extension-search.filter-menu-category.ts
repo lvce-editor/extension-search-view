d8caf8e018c7d7b4fe0ab19ee5c7946fa900c7d6
levivilet <72156503+levivilet@users.noreply.github.com>
2026-09-12 21:57:04 +0000

    test: use existing menu command instead of deprecated hover

--- packages/e2e/src/extension-search.filter-menu-category.ts
+++ packages/e2e/src/extension-search.filter-menu-category.ts
@@ -6,8 +6,7 @@ export const test: Test = async ({ Command, ContextMenu, expect, ExtensionSearch
   await ExtensionSearch.handleClickFilter()
 
   const menus = Locator('.Menu')
-  const category = menus.nth(0).locator('text=Category')
-  await category.hover()
+  await Command.execute('Menu.handleMouseEnter', 0, 6, 0, 0, 2)
   await expect(menus).toHaveCount(2)
   const subMenu = menus.nth(1)
   await expect(subMenu.locator('.MenuItem')).toHaveCount(20)
