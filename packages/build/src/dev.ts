78bde0380694b2be7d0b5fd7821ec2172caad0d4
Le Vivilet <levivilet@pm.me>
2026-09-13 19:33:56 +0200

    Update server dependency (#731)

--- packages/build/src/dev.ts
+++ packages/build/src/dev.ts
@@ -1,12 +1,17 @@
 import { execa } from 'execa'
+import { join } from 'node:path'
 import { root } from './root.ts'
 
 const main = async (): Promise<void> => {
+  await execa('npm', ['run', 'build'], {
+    cwd: root,
+    stdio: 'inherit',
+  })
   execa(`npm`, ['run', 'build:watch'], {
     cwd: root,
     stdio: 'inherit',
   })
-  execa('node', ['node_modules/@lvce-editor/server/bin/server.js', '--test-path=packages/e2e'], {
+  execa('node', ['node_modules/@lvce-editor/server/bin/server.js', '--link', join(root, '.tmp', 'dist'), '--test-path=packages/e2e'], {
     cwd: root,
     stdio: 'inherit',
   })
